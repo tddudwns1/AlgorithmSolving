bd1a0e0bdcb8c1a49c5620d6105568f08787903a
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-08-04 12:54:01 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 132 ms, Memory: 20,220 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,40 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		for (int tc = 1; tc <= 10; tc++) {
-			sb.append("#").append(tc).append(" ");
-			br.readLine();
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int[] password = new int[8];
-			int min = Integer.MAX_VALUE;
-			for (int i = 0; i < 8; i++) {
-				password[i] = Integer.parseInt(st.nextToken());
-				min = Math.min(min, password[i] / 15 - 1);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        while(true){
+        	try {
+                st = new StringTokenizer(br.readLine());
+			} catch (Exception e) {
+				break;
 			}
-			if (min > 0)
-				for (int i = 0; i < 8; i++)
-					password[i] %= 15 * min;
-			int sub = 1;
-			int i = 0;
-			while (true) {
-				password[i] -= sub;
-				if (password[i] <= 0) {
-					password[i] = 0;
-					for (int j = 0; j < 8; j++)
-						sb.append(password[++i % 8]).append(" ");
-					break;
-				}
-				i = (i + 1) % 8;
-				sub = sub % 5 + 1;
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb);
-	}
+        
+         if(!st.hasMoreElements())
+            break;
+        
+        String test_case = st.nextToken();
+        List<Integer> s = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 8; i++)
+                s.add(Integer.parseInt(st.nextToken()));
+        int cnt = 1;
+        while((s.get(0) - cnt) > 0) {
+            int temp = s.get(0) - cnt;
+            s.remove(0);
+            s.add(temp);
+            cnt++;
+            if(cnt > 5)
+                cnt = 1;
+        }
+        
+        s.remove(0);
+        s.add(0);
+        
+        System.out.print("#" + test_case + " ");
+        for(Integer i : s)
+            System.out.print(i + " ");
+        System.out.println();
+
+    }
 }
+}
+
