b28cc727455885b689890b3a6a95a8c418b85000
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-08-07 13:52:28 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 524 ms, Memory: 292336 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Queue<Integer> q = new LinkedList<>();
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken()) - 1;
+		Queue<Integer> ans = new LinkedList<>();
+
+		for (int i = 1; i <= n; i++)
+			q.add(i);
+		sb.append("<");
+		while (!q.isEmpty()) {
+			for (int i = 0; i < k; i++)
+				q.add(q.poll());
+			sb.append(q.poll()).append(", ");
+		}
+		sb.delete(sb.length() - 2, sb.length());
+		sb.append(">");
+		System.out.println(sb);
+	}
+}
