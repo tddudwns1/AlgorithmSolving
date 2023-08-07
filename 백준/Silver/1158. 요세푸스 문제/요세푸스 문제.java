5f1083f1e66e4a16f958bdd2b618e3f280fb672f
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-08-07 14:05:02 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 88 ms, Memory: 12240 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,17 +9,19 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		Queue<Integer> q = new LinkedList<>();
+		Stack<Integer> q = new Stack<>();
 		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken()) - 1;
+		int k = Integer.parseInt(st.nextToken());
 
-		for (int i = 1; i <= n; i++)
+		for (int i = n; i > 0; i--)
 			q.add(i);
 		sb.append("<");
+		int index = q.size();
 		while (!q.isEmpty()) {
-			for (int i = 0; i < k; i++)
-				q.add(q.poll());
-			sb.append(q.poll()).append(", ");
+			index -= k;
+			while(index < 0)
+				index += q.size();
+			sb.append(q.remove(index)).append(", ");
 		}
 		sb.delete(sb.length() - 2, sb.length());
 		sb.append(">");
