4140a418772dc9622a5607afb0bc45bd344d653a
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-08-07 16:06:46 +0900

    [Gold V] Title: 탑, Time: 836 ms, Memory: 129608 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -6,23 +6,48 @@ import java.util.Stack;
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
 		int[][] tops = new int[n][2];
 		String[] topstr = br.readLine().split(" ");
 		Stack<int[]> stack = new Stack<>();
-		for (int i = 0; i < n; i++)
-			tops[i] = new int[] { Integer.parseInt(topstr[i]), 0 };
-		stack.add(tops[n - 1]);
-		for (int i = n - 2; i >= 0; i--)
-			if (stack.isEmpty() || tops[i][0] <= stack.peek()[0])
-				stack.add(tops[i]);
-			else {
-				while (!stack.isEmpty() && stack.peek()[0] <= tops[i][0])
-					stack.pop()[1] = i + 1;
-				stack.add(tops[i]);
+		sb.append("0 ");
+		stack.add(new int[] { Integer.parseInt(topstr[0]), 1 });
+		for (int i = 1; i < n; i++) {
+			int h = Integer.parseInt(topstr[i]);
+			// 이전 > h
+			if (stack.peek()[0] > h) {
+				sb.append(stack.peek()[1]).append(" ");
+				stack.add(new int[] { h, i + 1 });
+				continue;
 			}
-
-		for (int i = 0; i < n; i++)
-			System.out.print(tops[i][1] + " ");
+			if (stack.peek()[0] == h) {
+				sb.append(stack.peek()[1]).append(" ");
+				stack.add(new int[] { h, i + 1 });
+				continue;
+			}
+			while (true) {
+				if (stack.isEmpty()) {
+					sb.append("0 ");
+					stack.add(new int[] { h, i + 1 });
+					break;
+				}
+				if (stack.peek()[0] < h) {
+					stack.pop();
+					continue;
+				}
+				if (stack.peek()[0] > h) {
+					sb.append(stack.peek()[1]).append(" ");
+					stack.add(new int[] { h, i + 1 });
+					break;
+				}
+				if (stack.peek()[0] == h) {
+					sb.append(stack.peek()[1]).append(" ");
+					stack.add(new int[] { h, i + 1 });
+					break;
+				}
+			}
+		}
+		System.out.println(sb);
 	}
 }
