f1688a572f39fe8b9fa4eff826ab0132ad0502b5
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-08-11 16:43:21 +0900

    [Gold V] Title: 치킨 배달, Time: 116 ms, Memory: 12020 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -6,6 +6,12 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 class Main {
+	static List<House> houses;
+	static List<Joint> joints;
+	static int m, jointCnt, houseCnt;
+	static int[] comb;
+	static int[][] distances;
+	
 	static class House {
 		int y;
 		int x;
@@ -26,59 +32,55 @@ class Main {
 		}
 	}
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
+		saveAddress();
+		calcDistances();
+		System.out.println(getMinDistance());
+	}
+	
+	private static void saveAddress() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		List<House> houses = new ArrayList<>();
-		List<Joint> joints = new ArrayList<>();
+		m = Integer.parseInt(st.nextToken());
+		houses = new ArrayList<>();
+		joints = new ArrayList<>();
 
 		for (int i = 0; i < n; i++) {
-			char[] line = br.readLine().toCharArray();
+			char[] line = br.readLine().toCharArray(); // why toCharArray
 			for (int j = 0; j < n; j++)
-				if (line[2 * j] == '1')
+				if (line[j * 2] == '1')
 					houses.add(new House(j, i));
-				else if (line[2 * j] == '2')
+				else if (line[j * 2] == '2')
 					joints.add(new Joint(j, i));
 		}
-		
-		int jointCnt = joints.size();
-		int houseCnt = houses.size();
-		int[][] distances = new int[jointCnt][houseCnt];
+	}
+
+	//치킨집마다 집 거리 저장
+	private static void calcDistances() {
+		jointCnt = joints.size();
+		houseCnt = houses.size();
+		distances = new int[jointCnt][houseCnt];
 		for (int i = 0; i < joints.size(); i++)
 			for (int j = 0; j < houses.size(); j++)
 				distances[i][j] = Math.abs(joints.get(i).x - houses.get(j).x) + Math.abs(joints.get(i).y - houses.get(j).y);
-		
-//		int[] nums = new int[n];
-//		for (int i = 0; i < n; i++)
-//			nums[i] = Integer.parseInt(st.nextToken());
-		int[] comb = new int[jointCnt];
+	}
+	
+	private static int getMinDistance() {
+		//치킨집 조합 
+		comb = new int[jointCnt];
 		for (int i = 1; i <= m; i++)
 			comb[jointCnt - i] = 1;
-
 		int minDIstance = Integer.MAX_VALUE;
 		do {
-			int sumDistance = 0;
-			for (int i = 0; i < houseCnt; i++) {
-				int nowDistance = Integer.MAX_VALUE;
-				for(int j = 0; j < jointCnt; j++) {
-					if (comb[j] == 0)
-						continue;
-					nowDistance = Math.min(nowDistance, distances[j][i]);
-				}
-				sumDistance += nowDistance;
-			}
-			minDIstance = Math.min(minDIstance, sumDistance);
+			minDIstance = Math.min(minDIstance, getSumDistance());
 		} while (np(comb));
-		
-		System.out.println(minDIstance);
+		return minDIstance;
 	}
 
 	private static boolean np(int[] comb) {
 		int i, j, k;
-		i = j = k = comb.length - 1;
+		i = j = k = jointCnt - 1;
 		
 		while(i > 0 && comb[i - 1] >= comb[i]) i--;
 		
@@ -97,5 +99,19 @@ class Main {
 		int tmp = comb[i];
 		comb[i] = comb[j];
 		comb[j] = tmp;
+	}
+	
+	private static int getSumDistance() {
+		int sumDistance = 0;
+		for (int i = 0; i < houseCnt; i++) {
+			int nowDistance = Integer.MAX_VALUE;
+			for(int j = 0; j < jointCnt; j++) {
+				if (comb[j] == 0)
+					continue;
+				nowDistance = Math.min(nowDistance, distances[j][i]);
+			}
+			sumDistance += nowDistance;
+		}
+		return sumDistance;
 	}
 }
