93806b851e3542401e6ccc5a24386ca18354a291
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-08-18 13:35:31 +0900

    [Gold IV] Title: 알파벳, Time: 844 ms, Memory: 12276 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,20 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int max = 0, r, c;
 	static int[] moveY = new int[] { 1, 0, -1, 0 };
 	static int[] moveX = new int[] { 0, 1, 0, -1 };
-	static Character[][] board;
-	static Set<Character> set = new HashSet<>();
+	static char[][] board;
 	static boolean[] visited = new boolean[26];
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -23,16 +16,16 @@ public class Main {
 		r = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
 
-		board = new Character[r + 2][c + 2];
-		for (int i = 1; i <= r; i++) {
+		board = new char[r][c];
+		for (int i = 0; i < r; i++) {
 			char[] line = br.readLine().toCharArray();
-			for (int j = 1; j <= c; j++) {
-				board[i][j] = line[j - 1];
+			for (int j = 0; j < c; j++) {
+				board[i][j] = line[j];
 			}
 		}
 
-		visited[board[1][1] - 'A'] = true;
-		dfs(1, 1, 1);
+		visited[board[0][0] - 'A'] = true;
+		dfs(0, 0, 1);
 
 		System.out.println(max);
 	}
@@ -41,10 +34,10 @@ public class Main {
 		for (int i = 0; i < 4; i++) {
 			int dy = y + moveY[i];
 			int dx = x + moveX[i];
-			if(dy < 1 || dx < 1 || dy > r || dx > c)
+			if(dy < 0 || dx < 0 || dy >= r || dx >= c)
 				continue;
 			
-			Character ch = board[dy][dx];
+			char ch = board[dy][dx];
 			if (!visited[ch - 'A']) {
 				visited[ch - 'A'] = true;
 				dfs(dy, dx, len + 1);
