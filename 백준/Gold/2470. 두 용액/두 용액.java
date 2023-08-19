1872ab3006ebafa4382b4b67a575084eee64cb0d
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-08-19 11:35:12 +0900

    [Gold V] Title: 두 용액, Time: 272 ms, Memory: 31268 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -2,19 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
+		int[] solution = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			solution[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(solution);
+		
 		int a = 0;
 		int b = 0;
 		int min = Integer.MAX_VALUE;
-		int[] solution = new int[n];
-		String[] str = br.readLine().split(" ");
-		for (int i = 0; i < n; i++)
-			solution[i] = Integer.parseInt(str[i]);
-		Arrays.sort(solution);
 		int left = 0;
 		int right = n - 1;
 		if (solution[0] >= 0) {
