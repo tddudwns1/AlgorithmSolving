480dc878f2f8cde24b42dc5e0125a4668c44687c
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-08-22 20:26:07 +0900

    [Gold V] Title: 암호 만들기, Time: 84 ms, Memory: 12124 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,49 +1,56 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	static char[] array;
+	static int l, c;
+	static char[] candidate, ans;
 	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
+	static char[] aeiou = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		
-		int l = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		array = new char[c];
-		for(int i = 0; i<c; i++) {
-			char alphabet = st.nextToken().charAt(0);
-			array[i] = alphabet;
-		}
-		
-		Arrays.sort(array);
-		
-		subSet(0, 0, 0, l, c, 0, "");
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		l = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		ans = new char[l];
+		candidate = new char[c];
+		candidate = br.readLine().replace(" ", "").toCharArray();
+		Arrays.sort(candidate);
+		int index = 0;
+		int conCnt = 2;
+		int vowCnt = 1;
+		int len = 0;
+		dfs(index, conCnt, vowCnt, len);
 		System.out.println(sb);
-		
-		
 	}
-	
-	public static void subSet(int count, int vowels, int consonant, int l, int c, int start, String pwd) {
-		if(count == l) { //기저 조건
-			
-			if(vowels > 0 && consonant > 1)
-				sb.append(pwd).append("\n");
+
+	private static void dfs(int index, int conCnt, int vowCnt, int len) {
+		if (l - len < conCnt || l - len < vowCnt)
+			return;
+
+		if (len == l) {
+			for (int i = 0; i < l; i++)
+				sb.append(ans[i]);
+			sb.append("\n");
 			return;
 		}
-		
-		for(int i = start; i<c; i++) {
-			if(array[i] == 'a' || array[i] == 'e' || array[i] == 'i' || array[i] == 'o' || array[i] == 'u')
-			{
-				subSet(count+1, vowels+1, consonant, l, c, i+1, pwd+array[i]);
-			}
-			else 
-				subSet(count+1, vowels, consonant+1, l, c, i+1, pwd+array[i]);
-	
+
+		for (int i = index; i < c; i++) {
+			ans[len] = candidate[i];
+			if (aeiou(candidate[i]))
+				dfs(i + 1, conCnt, vowCnt - 1, len + 1);
+			else
+				dfs(i + 1, conCnt - 1, vowCnt, len + 1);
 		}
+	}
+
+	private static boolean aeiou(char ch) {
+		for (int i = 0; i < 5; i++)
+			if (aeiou[i] == ch)
+				return true;
+		return false;
 	}
 }
