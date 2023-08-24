690b4bd6754c4482cecd0f8c02c0b302e22cc39f
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-08-24 23:20:25 +0900

    [Gold IV] Title: 회전 초밥, Time: 496 ms, Memory: 168704 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -5,51 +5,42 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    // 입력 받기
-    int n = Integer.parseInt(st.nextToken());
-    int d = Integer.parseInt(st.nextToken());
-    int k = Integer.parseInt(st.nextToken());
-    int c = Integer.parseInt(st.nextToken());
+		// 입력 받기
+		int n = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 
-    int cnt = 0, ans = 0, max = Integer.MIN_VALUE;
-    int[] chk = new int[d + 1];
-    int[] belt = new int[n];
-    chk[c]++;// 쿠폰 초밥 미리 추가
-    cnt++;
+		int cnt = 1, ans = 0, max = Integer.MIN_VALUE;
+		int[] chk = new int[d + 1];
+		int[] belt = new int[n];
+	    chk[c]++;// 쿠폰 초밥 미리 추가
 
-    for (int i = 0; i < n; i++) {
-        belt[i] = Integer.parseInt(br.readLine());// 값 받아오기
-    }
+		for (int i = 0; i < n; i++) {
+			belt[i] = Integer.parseInt(br.readLine());// 값 받아오기
+		}
 
-    for (int i = 0; i < n; i++) {
-        if (i == 0) {//첫 초밥
-            for (int j = 0; j < k; j++) {
-                if (chk[belt[j]] == 0) 
-                    cnt++;
-                chk[belt[j]]++;
-                
-            }
-        } else {
-            int last = belt[i - 1];
-            if (chk[last] == 1) {
-                cnt--;
-            }
-            chk[last]--;
-            int next_idx = i + k - 1;
-            if (next_idx >= n)
-                next_idx -= n;
-            if (chk[belt[next_idx]] == 0) {
-                cnt++;
-            }
-            chk[belt[next_idx]]++;
-        }
-        max = Math.max(max, cnt);
-    }
-    System.out.println(max);
-}
+		for (int j = 0; j < k; j++)
+			if (chk[belt[j]]++ == 0)
+				cnt++;
+
+		for (int i = 1; i < n; i++) {
+			int last = belt[i - 1];
+			if (chk[last]-- == 1)
+				cnt--;
+			int next_idx = i + k - 1;
+			if (next_idx >= n)
+				next_idx -= n;
+			if (chk[belt[next_idx]]++ == 0)
+				cnt++;
+
+			max = Math.max(max, cnt);
+		}
+		System.out.println(max);
+	}
 }
