8a7f53d3350bbc6d8e94250c24f2621d9a5fc6ea
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-09-01 01:07:31 +0900

    [Gold IV] Title: 스도쿠, Time: 304 ms, Memory: 13736 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -21,9 +21,10 @@ public class Main {
 				puzzle[i * 9 + j] = line[j];
 				if (line[j] == '0')
 					continue;
-				row[i] |= 1 << (line[j] - '0' - 1);
-				col[j] |= 1 << (line[j] - '0' - 1);
-				sec[(i / 3) * 3 + j / 3] |= 1 << (line[j] - '0' - 1);
+				int n = 1 << (line[j] - '0' - 1);
+				row[i] |= n;
+				col[j] |= n;
+				sec[(i / 3) * 3 + j / 3] |= n;
 			}
 		}
 
@@ -39,32 +40,37 @@ public class Main {
 	}
 
 	private static void bt(int cnt) {
-		if (cnt == 81) {
+//		if (cnt == 81) {
+//			flag = true;
+//			return;
+//		}
+		try {
+			if (puzzle[cnt] != '0')
+				bt(cnt + 1);
+			else {
+				int y = cnt / 9;
+				int x = cnt % 9;
+				int yx = (y / 3) * 3 + x / 3;
+				for (int i = 0; i < 9; i++) {
+					int n = 1 << i;
+					if ((row[y] & n) != 0 || (col[x] & n) != 0 || (sec[yx] & n) != 0)
+						continue;
+					row[y] |= n;
+					col[x] |= n;
+					sec[yx] |= n;
+					puzzle[y * 9 + x] = (char) (i + 1 + '0');
+					bt(cnt + 1);
+					if (flag)
+						return;
+					row[y] &= ~n;
+					col[x] &= ~n;
+					sec[yx] &= ~n;
+					puzzle[y * 9 + x] = '0';
+				}
+			}
+		} catch (Exception e) {
 			flag = true;
 			return;
-		}
-		if (puzzle[cnt] != '0')
-			bt(cnt + 1);
-		else {
-			int y = cnt / 9;
-			int x = cnt % 9;
-			int yx = (y / 3) * 3 + x / 3;
-			for (int i = 0; i < 9; i++) {
-				int n = 1 << i;
-				if ((row[y] & n) != 0 || (col[x] & n) != 0 || (sec[yx] & n) != 0)
-					continue;
-				row[y] |= n;
-				col[x] |= n;
-				sec[yx] |= n;
-				puzzle[y * 9 + x] = (char) (i + 1 + '0');
-				bt(cnt + 1);
-				if (flag)
-					return;
-				row[y] &= ~n;
-				col[x] &= ~n;
-				sec[yx] &= ~n;
-				puzzle[y * 9 + x] = '0';
-			}
 		}
 	}
 }
