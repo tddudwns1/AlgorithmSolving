edb35eb9b171fa8aa0090357f707989212e9c930
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-09-14 00:51:55 +0900

    [Silver I] Title: 숨바꼭질, Time: 192 ms, Memory: 93288 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -6,37 +6,32 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static boolean[] visited = new boolean[1_000_000];
+	static boolean[] visited = new boolean[200_000];
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
 		int cnt = 0;
 		visited[n] = true;
-//		if (n == 0) {
-//			n++;
-//			cnt++;
-//			visited[n] = true;
-//		}
 
-		System.out.println(bfs(n, m, cnt));
+		System.out.println(bfs(n, k, cnt));
 	}
 
-	private static int bfs(int n, int m, int cnt) {
+	private static int bfs(int n, int k, int cnt) {
 		Queue<int[]> q = new LinkedList<>();
 		q.add(new int[] { n, cnt });
 
 		while (!q.isEmpty()) {
 			int[] now = q.poll();
 			visited[now[0]] = true;
-			if (now[0] == m)
+			if (now[0] == k)
 				return now[1];
 			cnt++;
 			if (now[0] - 1 >= 0 && !visited[now[0] - 1])
 				q.add(new int[] { now[0] - 1, now[1] + 1 });
-			if (now[0] < m) {
+			if (now[0] < k) {
 				if (!visited[now[0] + 1])
 					q.add(new int[] { now[0] + 1, now[1] + 1 });
 				if (!visited[now[0] * 2])
