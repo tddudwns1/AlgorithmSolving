6a10c641d713d43582889d6f959ee36e8b5c023c
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-09-14 01:01:11 +0900

    [Silver I] Title: 숨바꼭질, Time: 124 ms, Memory: 19548 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -6,36 +6,43 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static boolean[] visited = new boolean[200_000];
+	static int[] visited = new int[199_999];
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		int cnt = 0;
-		visited[n] = true;
+		if (n == k) {
+			System.out.println(0);
+			return;
+		}
+		visited[n] = 1;
 
-		System.out.println(bfs(n, k, cnt));
+		System.out.println(bfs(n, k) - 1);
 	}
 
-	private static int bfs(int n, int k, int cnt) {
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] { n, cnt });
+	private static int bfs(int n, int k) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(n);
 
 		while (!q.isEmpty()) {
-			int[] now = q.poll();
-			visited[now[0]] = true;
-			if (now[0] == k)
-				return now[1];
-			cnt++;
-			if (now[0] - 1 >= 0 && !visited[now[0] - 1])
-				q.add(new int[] { now[0] - 1, now[1] + 1 });
-			if (now[0] < k) {
-				if (!visited[now[0] + 1])
-					q.add(new int[] { now[0] + 1, now[1] + 1 });
-				if (!visited[now[0] * 2])
-					q.add(new int[] { now[0] * 2, now[1] + 1 });
+			int now = q.poll();
+			if (now == k)
+				return visited[now];
+			if (now < k) {
+				if (visited[now * 2] == 0) {
+					q.add(now * 2);
+					visited[now * 2] = visited[now] + 1;
+				}
+				if (visited[now + 1] == 0) {
+					q.add(now + 1);
+					visited[now + 1] = visited[now] + 1;
+				}
+			}
+			if (now - 1 >= 0 && visited[now - 1] == 0) {
+				q.add(now - 1);
+				visited[now - 1] = visited[now] + 1;
 			}
 		}
 		return -1;
