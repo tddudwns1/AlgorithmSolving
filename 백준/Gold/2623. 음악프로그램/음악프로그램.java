c834b6c90fe54107c5b36917bd78c9e29f47ba8c
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-09-14 21:37:13 +0900

    [Gold III] Title: 음악프로그램, Time: 112 ms, Memory: 13844 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.java
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.java
@@ -6,61 +6,57 @@ import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-	static StringBuilder sb = new StringBuilder();
-	static boolean flag = false;
+        static StringBuilder sb = new StringBuilder();
+        static boolean flag = false;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
 
-		int[] check = new int[n + 1];
-		Deque<Integer>[] priority = new ArrayDeque[n + 1];
+            int[] check = new int[n + 1];
+            Deque<Integer>[] priority = new ArrayDeque[n + 1];
 
-		for (int i = 1; i <= n; i++) {
-			priority[i] = new ArrayDeque<>();
-		}
+            for (int i = 1; i <= n; i++)
+                priority[i] = new ArrayDeque<>();
 
-		for (int t = 0; t < m; t++) {
-			st = new StringTokenizer(br.readLine());
-			int k = Integer.parseInt(st.nextToken());
-			int[] ks = new int[k];
-			for (int i = 0; i < k; i++) {
-				ks[i] = Integer.parseInt(st.nextToken());
-				for (int j = 0; j < i; j++) {
-					priority[ks[i]].add(ks[j]);
-				}
-			}
-		}
-		for (int i = 1; i <= n; i++)
-			if (check[i] == 0)
-				dfs(priority, check, i);
+            for (int t = 0; t < m; t++) {
+                st = new StringTokenizer(br.readLine());
+                int k = Integer.parseInt(st.nextToken());
+                int[] ks = new int[k];
+                for (int i = 0; i < k; i++) {
+                    ks[i] = Integer.parseInt(st.nextToken());
+                    for (int j = 0; j < i; j++)
+                        priority[ks[i]].add(ks[j]);
+                }
+            }
+            for (int i = 1; i <= n; i++)
+                if (check[i] == 0)
+                    dfs(priority, check, i);
 
-		if (flag)
-			System.out.println(0);
-		else
-			System.out.println(sb);
-	}
+            if (flag)
+                System.out.println(0);
+            else
+                System.out.println(sb);
+        }
 
-	private static void dfs(Deque<Integer>[] priority, int[] check, int idx) {
-		if (check[idx] == 2 || flag) {
-			return;
-		}
+        private static void dfs(Deque<Integer>[] priority, int[] check, int idx) {
+            if (check[idx] == 2 || flag)
+                return;
 
-		if (check[idx] == 1) {
-			flag = true;
-			return;
-		}
+            if (check[idx] == 1) {
+                flag = true;
+                return;
+            }
 
-		if (!priority[idx].isEmpty())
-			check[idx] = 1;
+            if (!priority[idx].isEmpty())
+                check[idx] = 1;
 
-		while (!priority[idx].isEmpty()) {
-			dfs(priority, check, priority[idx].removeFirst());
-		}
+            while (!priority[idx].isEmpty())
+                dfs(priority, check, priority[idx].removeFirst());
 
-		check[idx] = 2;
-		sb.append(idx).append("\n");
-	}
-}
+            check[idx] = 2;
+            sb.append(idx).append("\n");
+        }
+    }
