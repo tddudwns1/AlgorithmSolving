d9b02bfbac1105ac433d13fbefb086eaa9154944
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-09-26 23:47:48 +0900

    [Silver II] Title: 오목, Time: 76 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Silver/2072. 오목/오목.java
+++ 백준/Silver/2072. 오목/오목.java
@@ -9,12 +9,12 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st;
+
 		int ans = -1;
 		int color = 0;
 		for (int i = 0; i < 17; i++) {
 			color = color != 1 ? 1 : 2;
-			st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int y = Integer.parseInt(st.nextToken());
 			int x = Integer.parseInt(st.nextToken());
 
@@ -22,7 +22,7 @@ public class Main {
 		}
 		for (int i = 17; i < n; i++) {
 			color = color != 1 ? 1 : 2;
-			st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int y = Integer.parseInt(st.nextToken());
 			int x = Integer.parseInt(st.nextToken());
 
