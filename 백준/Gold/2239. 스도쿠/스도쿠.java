12a9f89fad95bbf1a7d2078e89a35f6b5a28aa81
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-09-26 23:46:03 +0900

    [Gold IV] Title: 스도쿠, Time: 316 ms, Memory: 13796 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -3,13 +3,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int[] puzzle;
+	static char[] puzzle;
 	static int[] row, col, sec;
+	static int check = 1 << 9 - 1;
 	static boolean flag = false;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		puzzle = new int[81];
+		puzzle = new char[81];
 		row = new int[9];
 		col = new int[9];
 		sec = new int[9];
@@ -17,10 +18,10 @@ public class Main {
 		for (int i = 0; i < 9; i++) {
 			char[] line = br.readLine().toCharArray();
 			for (int j = 0; j < 9; j++) {
+				puzzle[i * 9 + j] = line[j];
 				if (line[j] == '0')
 					continue;
-				puzzle[i * 9 + j] = line[j] - '0';
-				int n = 1 << line[j] - '0';
+				int n = 1 << (line[j] - '0' - 1);
 				row[i] |= n;
 				col[j] |= n;
 				sec[(i / 3) * 3 + j / 3] |= n;
@@ -40,28 +41,28 @@ public class Main {
 
 	private static void bt(int cnt) {
 		try {
-			if (puzzle[cnt] != 0)
+			if (puzzle[cnt] != '0')
 				bt(cnt + 1);
 			else {
 				int y = cnt / 9;
 				int x = cnt % 9;
-				int z = (y / 3) * 3 + x / 3;
+				int yx = (y / 3) * 3 + x / 3;
 				int idx = y * 9 + x;
-				for (int i = 1; i <= 9; i++) {
+				for (int i = 0; i < 9; i++) {
 					int n = 1 << i;
-					if ((row[y] & n) != 0 || (col[x] & n) != 0 || (sec[z] & n) != 0)
+					if ((row[y] & n) != 0 || (col[x] & n) != 0 || (sec[yx] & n) != 0)
 						continue;
 					row[y] |= n;
 					col[x] |= n;
-					sec[z] |= n;
-					puzzle[idx] = i;
+					sec[yx] |= n;
+					puzzle[idx] = (char) (i + 1 + '0');
 					bt(cnt + 1);
 					if (flag)
 						return;
 					row[y] &= ~n;
 					col[x] &= ~n;
-					sec[z] &= ~n;
-					puzzle[idx] = 0;
+					sec[yx] &= ~n;
+					puzzle[idx] = '0';
 				}
 			}
 		} catch (Exception e) {
