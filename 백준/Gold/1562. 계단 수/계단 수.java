5db5d829d395fb084712b3fc803526301fe7ab93
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-10-11 22:16:09 +0900

    [Gold I] Title: 계단 수, Time: 92 ms, Memory: 18960 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.java
+++ 백준/Gold/1562. 계단 수/계단 수.java
@@ -1,57 +1,38 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-	private static final int MOD = 1000000000;
+	private static final int MOD = 10_0000_0000, LAST = (1 << 10) - 1;
+	private static Long[][][] dp;
 
-	private static Long[][][] cache;
-
-	private static long solve(int N, int usedNumbers, int length, int lastNumber) {
+	private static long solve(int n, int usedNumbers, int length, int lastNumber) {
 		usedNumbers |= (1 << lastNumber);
-		if (length == N) {
-			if (usedNumbers == ((1 << 10) - 1) && lastNumber != 0) {
+		if (++length == n) {
+			if (usedNumbers == LAST && lastNumber != 0)
 				return 1;
-			} else {
-				return 0;
-			}
+			return 0;
 		}
 
-		if (cache[usedNumbers][length][lastNumber] == null) {
-			long result = 0;
-
-			if (lastNumber > 0) {
-				result += solve(N, usedNumbers, length + 1, lastNumber - 1) % MOD;
-			}
-
-			if (lastNumber < 9) {
-				result += solve(N, usedNumbers, length + 1, lastNumber + 1) % MOD;
-			}
-
-			cache[usedNumbers][length][lastNumber] = result % MOD;
-		}
-
-		return cache[usedNumbers][length][lastNumber];
-	}
-
-	private static long solve(int N) {
+		if (dp[usedNumbers][length][lastNumber] != null)
+			return dp[usedNumbers][length][lastNumber];
+		
 		long result = 0;
-
-		for (int i = 0; i < 10; ++i) {
-			result += solve(N, 1 << i, 1, i) % MOD;
-		}
-
-		return result % MOD;
+		if (lastNumber > 0)
+			result += solve(n, usedNumbers, length, lastNumber - 1) % MOD;
+		if (lastNumber < 9)
+			result += solve(n, usedNumbers, length, lastNumber + 1) % MOD;
+		return dp[usedNumbers][length][lastNumber] = result % MOD;
 	}
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		dp = new Long[1 << 10][n][10];
 
-		int N = Integer.parseInt(in.readLine());
+		long result = 0;
+		for (int i = 0; i < 10; ++i)
+			result += solve(n, 1 << i, 0, i) % MOD;
 
-		cache = new Long[1 << 10][N][10];
-
-		System.out.println(solve(N));
-
-		in.close();
+		System.out.println(result % MOD);
 	}
 }
