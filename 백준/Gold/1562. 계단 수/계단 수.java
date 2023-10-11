ce77e29b4e1763c72511722e44595e6ad6ace6cb
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-10-11 22:39:35 +0900

    [Gold I] Title: 계단 수, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.java
+++ 백준/Gold/1562. 계단 수/계단 수.java
@@ -1,38 +1,53 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class Main {
-	private static final int MOD = 10_0000_0000, LAST = (1 << 10) - 1;
-	private static Long[][][] dp;
-
-	private static long solve(int n, int usedNumbers, int length, int lastNumber) {
-		usedNumbers |= (1 << lastNumber);
-		if (++length == n) {
-			if (usedNumbers == LAST)
-				return 1;
-			return 0;
-		}
-
-		if (dp[length][lastNumber][usedNumbers] != null)
-			return dp[length][lastNumber][usedNumbers];
+	
+	public static void main(String[] args) throws IOException {
 		
-		long result = 0;
-		if (lastNumber > 0)
-			result += solve(n, usedNumbers, length, lastNumber - 1) % MOD;
-		if (lastNumber < 9)
-			result += solve(n, usedNumbers, length, lastNumber + 1) % MOD;
-		return dp[length][lastNumber][usedNumbers] = result % MOD;
-	}
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		dp = new Long[n][10][1 << 10];
-
-		long result = 0;
-		for (int i = 1; i < 10; ++i) 
-			result += solve(n, 1 << i, 0, i) % MOD;
-
-		System.out.println(result % MOD);
+		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int MOD = 1000000000;
+		int num = Integer.parseInt(buf.readLine());
+		int[][][] memo = new int[num][10][4];
+		
+		memo[0][0][0] = 1;
+		for (int i = 1; i < 9; i ++) {
+			memo[0][i][1] = 1;
+		}
+		memo[0][9][2] = 1;
+		
+		for (int i = 1; i < num; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (j == 0) {
+					memo[i][0][3] = (memo[i-1][1][3] + memo[i-1][1][2]) % MOD;
+					memo[i][0][0] = (memo[i-1][1][0] + memo[i-1][1][1]) % MOD;
+				}
+				else if (j == 9) {
+					memo[i][9][3] = (memo[i-1][8][0] + memo[i-1][8][3]) % MOD;
+					memo[i][9][2] = (memo[i-1][8][1] + memo[i-1][8][2]) % MOD;
+				}
+				else {
+					memo[i][j][0] = (memo[i-1][j-1][0] + memo[i-1][j+1][0]) % MOD;
+					memo[i][j][1] = (memo[i-1][j-1][1] + memo[i-1][j+1][1]) % MOD;
+					memo[i][j][2] = (memo[i-1][j-1][2] + memo[i-1][j+1][2]) % MOD;
+					memo[i][j][3] = (memo[i-1][j-1][3] + memo[i-1][j+1][3]) % MOD;
+				}
+			}
+		}
+		
+		int result = 0;
+		
+		for (int i = 1; i < 10; i++) {
+			result = (result + memo[num-1][i][3]) % MOD;
+		}
+		
+		System.out.println(result);
+		
+		buf.close();
 	}
 }
