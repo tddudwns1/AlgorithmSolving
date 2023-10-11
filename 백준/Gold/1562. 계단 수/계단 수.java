f2097b4ff5cd1cdcbe31f45c683aae3b627d245c
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-10-11 22:23:40 +0900

    [Gold I] Title: 계단 수, Time: 92 ms, Memory: 17044 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.java
+++ 백준/Gold/1562. 계단 수/계단 수.java
@@ -8,29 +8,29 @@ public class Main {
 	private static long solve(int n, int usedNumbers, int length, int lastNumber) {
 		usedNumbers |= (1 << lastNumber);
 		if (++length == n) {
-			if (usedNumbers == LAST && lastNumber != 0)
+			if (usedNumbers == LAST)
 				return 1;
 			return 0;
 		}
 
-		if (dp[usedNumbers][length][lastNumber] != null)
-			return dp[usedNumbers][length][lastNumber];
+		if (dp[length][lastNumber][usedNumbers] != null)
+			return dp[length][lastNumber][usedNumbers];
 		
 		long result = 0;
 		if (lastNumber > 0)
 			result += solve(n, usedNumbers, length, lastNumber - 1) % MOD;
 		if (lastNumber < 9)
 			result += solve(n, usedNumbers, length, lastNumber + 1) % MOD;
-		return dp[usedNumbers][length][lastNumber] = result % MOD;
+		return dp[length][lastNumber][usedNumbers] = result % MOD;
 	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		dp = new Long[1 << 10][n][10];
+		dp = new Long[n][10][1 << 10];
 
 		long result = 0;
-		for (int i = 0; i < 10; ++i)
+		for (int i = 1; i < 10; ++i) 
 			result += solve(n, 1 << i, 0, i) % MOD;
 
 		System.out.println(result % MOD);
