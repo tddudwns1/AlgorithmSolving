03c753f92ac0563bdfaa013e198ec7e1bcc648c5
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-12-10 18:29:39 +0900

    [Silver III] Title: Java vs C++, Time: 80 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Silver/3613. Java vs C＋＋/Java vs C＋＋.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		System.out.println(convertor(new BufferedReader(new InputStreamReader(System.in)).readLine()));
+	}
+
+	private static String convertor(String input) {
+		StringBuilder sb = new StringBuilder();
+		String type = "";
+		int index = 0;
+		char now = input.charAt(index);
+
+		if (now < 97)
+			return "Error!";
+		sb.append(now);
+
+		while (input.length() > ++index) {
+			now = input.charAt(index);
+			if (now >= 97) {
+				sb.append(now);
+				continue;
+			}
+			if (now == '_')
+				type = "C++";
+			else
+				type = "Java";
+			break;
+		}
+
+		if (type.equals("C++")) {
+			while (input.length() > index) {
+				now = input.charAt(index++);
+				if (now >= 97) {
+					sb.append(now);
+					continue;
+				}
+				if (now != '_')
+					return "Error!";
+
+				if(input.length() <= index)
+					return "Error!";
+				
+				if(input.charAt(index) < 97)
+					return "Error!";
+
+				sb.append((char) (input.charAt(index++) - 32));
+			}
+		} else if (type.equals("Java")) {
+			while (input.length() > index) {
+				now = input.charAt(index);
+				if (now >= 97) {
+					sb.append(now);
+					index++;
+					continue;
+				}
+				
+				if (now == '_')
+					return "Error!";
+
+				sb.append('_');
+				sb.append((char) (input.charAt(index) + 32));
+				index++;
+			}
+		}
+
+		return sb.toString();
+	}
+}
