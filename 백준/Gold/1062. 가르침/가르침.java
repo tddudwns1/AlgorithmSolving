21978cd1ddda4e5a8a0f8806e570d6bae89fead3
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-12-15 02:06:35 +0900

    [Gold IV] Title: 가르침, Time: 108 ms, Memory: 11936 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, k, max = 0;
+    static int n, k, max = 0, target = 0;
     static int[] words;
 
     public static void main(String[] args) throws IOException {
@@ -26,34 +26,41 @@ public class Main {
             words[i] = alpha;
             for (int j = 4; j < word.length() - 4; j++)
                 words[i] |= 1 << (word.charAt(j) - '`');
+            target |= words[i];
         }
+
+        target ^= alpha;
 
         System.out.println(checkCanTeach(alpha));
     }
 
     private static int checkCanTeach(int alpha) {
-        if(k < 5)
+        if (k < 5)
             return 0;
-        if(k == 26)
+        if (k == 26)
+            return n;
+        int count = 0;
+        for(int i = 1; i <= 26; i++)
+            if ((target & (1 << i)) > 0)
+                count++;
+        if(count < k - 5)
             return n;
         countCanTeach(2, 5, alpha);
         return max;
     }
 
     private static void countCanTeach(int index, int count, int key) {
-        if(k == count){
+        if (k == count) {
             int total = 0;
-            for(int i = 0; i < n; i++)
-                if(words[i] == (key & words[i]))
+            for (int i = 0; i < n; i++)
+                if (words[i] == (key & words[i]))
                     total++;
             max = Integer.max(max, total);
             return;
         }
 
-        for(int i = index; i <= 26; i++){
-            if((key & (1 << i) ) > 0)
-                continue;
-            countCanTeach(i + 1, count + 1, key | (1 << i));
-        }
+        for (int i = index; i <= 26; i++)
+            if ((target & (1 << i)) > 0)
+                countCanTeach(i + 1, count + 1, key | (1 << i));
     }
 }
