8c67ea4f3bd9e4c506eef31da63683de65977e2d
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-12-22 00:02:13 +0900

    [Gold V] Title: 최소비용 구하기, Time: 356 ms, Memory: 46144 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,69 +1,83 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import org.w3c.dom.Node;
 
-class Main {
-	static class Router implements Comparable<Router> {
-		int index;
-		int height;
+import java.io.*;
+import java.util.*;
 
-		public Router(int index, int height) {
-			this.index = index;
-			this.height = height;
-		}
+public class Main {
+    static class Bus implements Comparable<Bus> {
+        int station;
+        int cost;
 
-		@Override
-		public int compareTo(Router o) {
-			return this.height - o.height;
-		}
+        public Bus(int destination, int cost) {
+            this.station = destination;
+            this.cost = cost;
+        }
 
-	}
+        @Override
+        public int compareTo(Bus o) {
+            return cost - o.cost;
+        }
+    }
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
-		int[][] costs = new int[n + 1][n + 1];
-		for (int i = 1; i <= n; i++)
-			for (int j = 1; j <= n; j++)
-				costs[i][j] = Integer.MAX_VALUE;
-		int[] lowCosts = new int[n + 1];
-		boolean[] visited = new boolean[n + 1];
-		Arrays.fill(lowCosts, Integer.MAX_VALUE);
-		for (int i = 0; i < m; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int y = Integer.parseInt(st.nextToken());
-			int x = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			if (costs[y][x] > cost)
-				costs[y][x] = cost;
-		}
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
-		dijkstra(start, costs, lowCosts, n, visited);
-		System.out.println(lowCosts[end]);
-	}
+        int[][] cities = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++)
+            Arrays.fill(cities[i], Integer.MAX_VALUE);
 
-	private static void dijkstra(int start, int[][] costs, int[] lowCosts, int n, boolean[] visited) {
-		PriorityQueue<Router> q = new PriorityQueue<>();
-		lowCosts[start] = 0;
-		q.add(new Router(start, lowCosts[start]));
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-		while (!q.isEmpty()) {
-			Router now = q.poll();
-			for (int i = 1; i <= n; i++) {
-				if (costs[now.index][i] == Integer.MAX_VALUE)
-					continue;
-				if (visited[i])
-					continue;
-				if (lowCosts[i] > now.height + costs[now.index][i])
-					q.add(new Router(i, lowCosts[i] = now.height + costs[now.index][i]));
-			}
-		}
-	}
+            cities[from][to] = Integer.min(cities[from][to], cost);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        System.out.println(dijkstra(start, end, n, cities));
+    }
+
+    private static int dijkstra(int start, int end, int n, int[][] cities) {
+        Queue<Bus> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[n + 1];
+
+        int[] costs = new int[n + 1];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+
+        pq.add(new Bus(start, 0));
+        costs[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Bus now = pq.poll();
+
+            if (visited[now.station])
+                continue;
+            visited[now.station] = true;
+
+            for (int i = 1; i <= n; i++) {
+                if(cities[now.station][i] == Integer.MAX_VALUE)
+                    continue;
+
+                if(visited[i])
+                    continue;
+
+                if(costs[i] <= costs[now.station] + cities[now.station][i])
+                    continue;
+
+                costs[i] = costs[now.station] + cities[now.station][i];
+                
+                pq.add(new Bus(i, costs[i]));
+            }
+        }
+
+        return costs[end];
+    }
 }
