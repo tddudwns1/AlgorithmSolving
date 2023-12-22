146dfeb49f586710623bb8582ad444805386c68a
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-12-22 11:50:49 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 608 ms, Memory: 55344 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,90 +1,66 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int ans = 0;
-	static class Edge implements Comparable<Edge>{
-		int end;
-		int weight;
-		public Edge(int end, int weight) {
-			this.end = end;
-			this.weight = weight;
-		}
-		
-		@Override
-		public int compareTo(Edge e) {
-			return this.weight - e.weight;
-		}
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int v = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
-		List<Edge>[] edges = new ArrayList[v + 1];
-		for(int i = 0; i <= v; i++) edges[i] = new ArrayList<Edge>();
-		for (int i = 0; i < e; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			
-			edges[a].add(new Edge(b, weight));
-			edges[b]
-					.add(
-							new Edge(a, weight));
-		}
-		
-		prim(edges, v);
-		
-		System.out.println(ans);
-	}
+    static class Edge implements Comparable<Edge> {
+        int node;
+        int weight;
 
-	private static void prim(List<Edge>[] edges, int v) {
-		boolean[] visited = new boolean[v + 1];
-		Queue<Edge> pq = new PriorityQueue<>();
-		pq.add(new Edge(1, 0));
-		
-		while(!pq.isEmpty()) {
-			Edge now = pq.poll();
-			if(visited[now.end])
-				continue;
-			visited[now.end] = true;
-			ans+=now.weight;
-			for(Edge e : edges[now.end]) {
-				if(visited[e.end])
-					continue;
-				pq.add(e);
-			}
-		}
-	}
-//	private static void prim(List<Edge>[] edges, int v) {
-//		boolean[] visited = new boolean[v + 1];
-//		Queue<Edge> pq = new PriorityQueue<>();
-//		pq.add(new Edge(1,0));
-//        
-//		while(!pq.isEmpty()) {
-//			Edge p = pq.poll();
-//			int node = p.end;
-//			int weight = p.weight;
-//			
-//			if(visited[node]) continue;
-//			// 선택한 간선의 정점으로부터 가장 낮은 가중치 갖는 정점 선택 
-//			visited[node]= true;
-//			ans += weight;
-//			
-//			for(Edge next : edges[node]) {
-//				if(!visited[next.end]) {
-//					pq.add(next);
-//				}
-//			}
-//		}
-//	}
+        public Edge(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return weight - o.weight;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        Deque<Edge>[] edges = new Deque[v + 1];
+        for (int i = 1; i <= v; i++)
+            edges[i] = new ArrayDeque<>();
+
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            edges[from].add(new Edge(to, weight));
+            edges[to].add(new Edge(from, weight));
+        }
+
+        System.out.println(prim(edges, v));
+    }
+
+    private static int prim(Deque<Edge>[] edges, int v) {
+        Queue<Edge> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[v + 1];
+
+        pq.add(new Edge(1, 0));
+
+        int answer = 0;
+        int count = v;
+        while (count > 0) {
+            Edge now = pq.poll();
+
+            if (visited[now.node])
+                continue;
+            visited[now.node] = true;
+            answer += now.weight;
+            count--;
+
+            for (Edge next : edges[now.node])
+                pq.add(next);
+        }
+
+        return answer;
+    }
 }
