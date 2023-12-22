9e880a5c1a11e4d5074c6b76d11ecbe63944d855
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-12-22 12:50:48 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 596 ms, Memory: 55796 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -6,11 +6,21 @@ public class Main {
         int node;
         int weight;
 
+        /**
+         * 연결된 정점 정보를 가지고 있는 객체
+         * @param node 반대편 정점
+         * @param weight 가중치
+         */
         public Edge(int node, int weight) {
             this.node = node;
             this.weight = weight;
         }
 
+        /**
+         * 낮은 비용을 우선시 하여 비교하는 함수
+         * @param o the object to be compared.
+         * @return 낮은 비용
+         */
         @Override
         public int compareTo(Edge o) {
             return weight - o.weight;
@@ -23,6 +33,7 @@ public class Main {
         int v = Integer.parseInt(st.nextToken());
         int e = Integer.parseInt(st.nextToken());
 
+        // 연결된 정보를 담을 큐 배열
         Deque<Edge>[] edges = new Deque[v + 1];
         for (int i = 1; i <= v; i++)
             edges[i] = new ArrayDeque<>();
@@ -33,6 +44,7 @@ public class Main {
             int to = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
 
+            // 양방향이므로 두 곳 모두 넣어준다
             edges[from].add(new Edge(to, weight));
             edges[to].add(new Edge(from, weight));
         }
@@ -41,22 +53,33 @@ public class Main {
     }
 
     private static int prim(Deque<Edge>[] edges, int v) {
+        // 프림 알고리즘을 위한 우선순위 큐와, 비용이 확정된 정점을 기록할 배열
         Queue<Edge> pq = new PriorityQueue<>();
         boolean[] visited = new boolean[v + 1];
 
+        // 1. 1번 정점부터 시작
         pq.add(new Edge(1, 0));
 
+        // 정답을 저장할 변수와, 모든 정점이 이어졌는지 확인 할 카운트 변수
         int answer = 0;
         int count = v;
+        // 6.(끝) 모든 정점이 이어질 때 까지
         while (count > 0) {
+            // 2. 우선순위 큐에서 현재 가장 가중치가 낮은 연결 정보를 가진 객체를 반환
             Edge now = pq.poll();
 
+            // 3. 이미 비용이 확정된 정점인지 확인
             if (visited[now.node])
+                // 3-1. 확정됐다면 건너뛰기
                 continue;
+            // 3-2. 확정되지 않았다면 기록
             visited[now.node] = true;
+
+            // 4. 정답 변수에 해당 가중치 추가, 카운트 변수 감소
             answer += now.weight;
             count--;
 
+            // 5. 해당 정점이 가진 연결 정보를 우선순위 큐에 추가
             for (Edge next : edges[now.node])
                 pq.add(next);
         }
