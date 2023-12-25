a84f6fc6d4fb4c5cfe5bab19254b803b5f3f2e6f
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-12-25 16:10:50 +0900

    [Gold III] Title: 줄 세우기, Time: 380 ms, Memory: 44188 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,59 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Student {
-		int front;
-		Deque<Integer> backs;
+    static StringBuilder sb = new StringBuilder();
+    static Student[] students;
+    static boolean[] checked;
 
-		public Student(int front) {
-			super();
-			this.front = front;
-			this.backs = new ArrayDeque<>();
-		}
+    static class Student {
+        Queue<Integer> taller = new ArrayDeque<>();
 
-	}
+        public Student() {
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		Student[] students = new Student[n + 1];
-		for (int i = 1; i <= n; i++)
-			students[i] = new Student(i);
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-			int idx = Integer.parseInt(st.nextToken());
-			int target = Integer.parseInt(st.nextToken());
+        students = new Student[n + 1];
+        for (int i = 1; i <= n; i++)
+            students[i] = new Student();
 
-			students[idx].front = target;
-			students[target].backs.add(idx);
-		}
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int taller = Integer.parseInt(st.nextToken());
+            int shorter = Integer.parseInt(st.nextToken());
 
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i <= n; i++)
-			print(i, students, sb);
+            students[shorter].taller.add(taller);
+        }
 
-		System.out.println(sb);
-	}
+        checked = new boolean[n + 1];
 
-	private static void print(int i, Student[] students, StringBuilder sb) {
-		if(students[i].front == 0)
-			return;
-		while(!students[i].backs.isEmpty()) {
-			print(students[i].backs.removeFirst(), students, sb);
-		}
-//		System.out.println(i);
-		sb.append(i).append(" ");
-		if(students[i].front != i)
-			students[students[i].front].backs.remove(i);
-		students[i].front = 0;
-	}
+        for (int i = 1; i <= n; i++) {
+            lineUp(i);
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void lineUp(int now) {
+        if (checked[now])
+            return;
+        while (!students[now].taller.isEmpty()) {
+            int taller = students[now].taller.poll();
+            lineUp(taller);
+        }
+        checked[now] = true;
+        sb.append(now).append(" ");
+    }
 }
