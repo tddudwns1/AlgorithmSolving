df1dd5dcc5e6094a8397df1cb74c5919836de3d9
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-04-21 02:29:35 +0900

    [Silver III] Title: Four Squares, Time: 104 ms, Memory: 11968 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] count = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+
+            for (int j = 1; j * j <= i; j++)
+                min = Math.min(min, count[i - j * j]);
+
+            count[i] = min + 1;
+        }
+
+        System.out.println(count[n]);
+    }
+}
