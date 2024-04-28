5a65adff523d145807d156e874f8d99aa8d0a9e6
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-04-28 12:23:21 +0900

    [Silver I] Title: IOIOI, Time: 192 ms, Memory: 28788 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,43 +1,44 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
-
-    public static void main(String[] args) throws Exception {
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
         int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        String s = br.readLine();
-        
-        sb.append("I");
-        for (int i=0; i<n; i++) {
-            sb.append("OI");
-        }
-        String p = sb.toString();
-        
-        int end = 2 * n + 1;
-        sb = new StringBuilder();
-        sb.append(s.substring(0, end));
-//        for (int i=0; i<end; i++) {
-//            sb.append(s[i]);
-//        }
-        
-        int answer = 0;
-        if (sb.toString().equals(p)) {
-            answer++;
-        }
-        
-        for (int i=end; i<m; i++) {
-            sb.deleteCharAt(0);
-            sb.append(s.charAt(i));
-            
-            if (sb.toString().equals(p)) {
-                answer++;
+        int s = Integer.parseInt(br.readLine());
+        String str = br.readLine() + " ";
+
+        int index = 0;
+        int count = 0;
+        portal:
+        while (index < s) {
+            if (str.charAt(index++) != 'I')
+                continue;
+
+            for (int j = 0; j < n - 1; j++) {
+                if (str.charAt(index) != 'O')
+                    continue portal;
+
+                index++;
+
+                if (str.charAt(index++) != 'I')
+                    continue portal;
+            }
+
+            while (true) {
+                if (str.charAt(index) != 'O')
+                    continue portal;
+
+                index++;
+
+                if (str.charAt(index++) != 'I')
+                    continue portal;
+
+                count++;
             }
         }
-        
-        System.out.println(answer);
-    }
 
+        System.out.println(count);
+    }
 }
