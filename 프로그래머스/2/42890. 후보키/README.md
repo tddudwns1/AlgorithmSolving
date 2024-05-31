d2f625b7af0dcc7636c8f537d7108674dc38c93c
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-05-31 23:32:03 +0900

    [level 2] Title: 후보키, Time: 2.20 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/README.md
+++ 프로그래머스/2/42890. 후보키/README.md
@@ -1,10 +1,10 @@
 # [level 2] 후보키 - 42890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890?language=java#) 
 
 ### 성능 요약
 
-메모리: 71.4 MB, 시간: 1.86 ms
+메모리: 80.1 MB, 시간: 2.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 31일 23:09:16
+2024년 05월 31일 23:31:59
 
 ### 문제 설명
 
--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
@@ -7,10 +6,8 @@ class Solution {
     
     public int solution(String[][] relation) {
         answer = 0;
-        
         row = relation.length;
         col = relation[0].length;
-        
         isChecked = new ArrayList<>();
         
         for (int i = 1; i <= col; i++) {
@@ -20,47 +17,62 @@ class Solution {
         return answer;
     }
     
-    public void process(int index, int nowChecked, int depth, String[][] relation) {
+    private void process(int index, int nowChecked, int depth, String[][] relation) {
         if (Integer.bitCount(nowChecked) == depth) {
-            Set<String> set = new HashSet<>();
-            Stack<Integer> stack = new Stack<>();
-            int length = Integer.SIZE - Integer.numberOfLeadingZeros(nowChecked);
-            for (int i = 0; i < length; i++)
-                if ((nowChecked & (1 << i)) != 0)
-                    stack.add(i);
+            if (!isUnique(nowChecked, relation)) return;
+            if (!isMinimal(nowChecked)) return;
             
-            for (int i = 0; i < row; i++) {
-                StringBuilder sb = new StringBuilder();
-                
-                for (int now : stack) {
-                    sb.append(relation[i][now]).append(",");
-                }
-                
-                set.add(sb.toString());
-            }
-            
-            if (set.size() < row)
-                return;
-                
-            for (int i = 0; i < isChecked.size(); i++)
-                if(isChecked.get(i) == (nowChecked & isChecked.get(i)))
-                    return;
-                
             isChecked.add(nowChecked);
-            
             answer++;
-            nowChecked = 0;
             return;
         }
         
-        portal: for (int i = index; i < col; i++) {
-            for (int j = 0; j < isChecked.size(); j++)
-                if((1 << i) == (i & isChecked.get(j)))
-                    continue portal;
+        for (int i = index; i < col; i++) {
+            if (isPartOfExistingCombination(i)) continue;
             
-            nowChecked = nowChecked | 1 << i;
+            nowChecked |= 1 << i;
             process(i + 1, nowChecked, depth, relation);
-            nowChecked = nowChecked & ~(1 << i);
+            nowChecked &= ~(1 << i);
         }
+    }
+    
+    private boolean isUnique(int nowChecked, String[][] relation) {
+        Set<String> set = new HashSet<>();
+        Stack<Integer> stack = new Stack<>();
+        int length = Integer.SIZE - Integer.numberOfLeadingZeros(nowChecked);
+        
+        for (int i = 0; i < length; i++) {
+            if ((nowChecked & (1 << i)) != 0) {
+                stack.add(i);
+            }
+        }
+        
+        for (int i = 0; i < row; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int now : stack) {
+                sb.append(relation[i][now]).append(",");
+            }
+            set.add(sb.toString());
+        }
+        
+        return set.size() == row;
+    }
+    
+    private boolean isMinimal(int nowChecked) {
+        for (int checked : isChecked) {
+            if ((nowChecked & checked) == checked) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isPartOfExistingCombination(int i) {
+        for (int checked : isChecked) {
+            if ((1 << i) == (i & checked)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
