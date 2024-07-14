fed1940c5f149ca745087b8377707510fa1774ef
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-07-14 19:12:43 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,6 +1,5 @@
--- 코드를 입력하세요
-SELECT i.animal_id as animal_id, i.name as name
-from animal_ins i
-join animal_outs o using (animal_id)
-order by DATEDIFF(o.DATETIME, i.DATETIME) DESC
+select i.animal_id as animal_id, i.name as name
+from animal_outs o
+join animal_ins i using (animal_id)
+order by datediff(o.datetime, i.datetime) desc
 limit 2;
