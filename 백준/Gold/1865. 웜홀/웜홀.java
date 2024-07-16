b8e45af4550776d01a73a5dc8fbe2ddbab4f0913
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-07-17 01:08:33 +0900

    [Gold III] Title: 웜홀, Time: 328 ms, Memory: 19540 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -28,9 +28,7 @@ public class Main {
             int m = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            int[][] infos = new int[n + 1][n + 1];
-            for (int i = 1; i <= n; i++)
-                Arrays.fill(infos[i], Integer.MAX_VALUE);
+            Queue<Edge> edges = new ArrayDeque<>();
 
             for (int i = 0; i < m; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -39,7 +37,8 @@ public class Main {
                 int e = Integer.parseInt(st.nextToken());
                 int t = Integer.parseInt(st.nextToken());
 
-                infos[e][s] = infos[s][e] = Math.min(infos[s][e], t);
+                edges.add(new Edge(s, e, t));
+                edges.add(new Edge(e, s, t));
             }
 
             for (int i = 0; i < w; i++) {
@@ -49,14 +48,8 @@ public class Main {
                 int e = Integer.parseInt(st.nextToken());
                 int t = Integer.parseInt(st.nextToken());
 
-                infos[s][e] = Math.min(infos[s][e], -t);
+                edges.add(new Edge(s, e, -t));
             }
-
-            Queue<Edge> edges = new ArrayDeque<>();
-            for (int i = 1; i <= n; i++)
-                for (int j = 1; j <= n; j++)
-                    if (infos[i][j] != Integer.MAX_VALUE)
-                        edges.add(new Edge(i, j, infos[i][j]));
 
             sb.append(bellmanFord(edges, n)).append("\n");
         }
@@ -78,7 +71,7 @@ public class Main {
                 times[edge.end] = times[edge.start] + edge.cost;
                 cantReturn = false;
             }
-            
+
             if (cantReturn)
                 return "NO";
         }
