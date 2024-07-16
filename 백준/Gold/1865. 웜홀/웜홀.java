755f524f905888675cbf174da1fe3c54b9b0615d
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-07-17 01:48:31 +0900

    [Gold III] Title: 웜홀, Time: 228 ms, Memory: 19312 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -60,16 +60,17 @@ public class Main {
 
     private static String bellmanFord(Edge[] edges, int n) {
         int[] times = new int[n + 1];
-        Arrays.fill(times, 500 * 10_000);
+        Arrays.fill(times, 5_000_000);
         times[0] = 0;
 
         for (int cycle = 1; cycle < n; cycle++) {
             boolean cantReturn = true;
             for (Edge edge : edges) {
-                if (times[edge.end] <= times[edge.start] + edge.cost)
+                int candidate = times[edge.start] + edge.cost;
+                if (times[edge.end] <= candidate)
                     continue;
 
-                times[edge.end] = times[edge.start] + edge.cost;
+                times[edge.end] = candidate;
                 cantReturn = false;
             }
 
