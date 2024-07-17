9498eb981ed9c86a647a8a8bfcedda8b4c0136ca
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-07-17 12:12:53 +0900

    [Gold IV] Title: 타임머신, Time: 196 ms, Memory: 16528 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+class Edge {
+    int s;
+    int e;
+    int t;
+
+    public Edge(int s, int e, int t) {
+        this.s = s;
+        this.e = e;
+        this.t = t;
+    }
+}
+
+public class Main {
+    static final Long MAX = 30_0000_0000L;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Edge[] edges = new Edge[m];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+
+            edges[i] = new Edge(s, e, t);
+        }
+
+        long[] times = new long[n + 1];
+        Arrays.fill(times, MAX);
+
+        if (bellmanFord(edges, times, n)){
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 2; i <= n; i++) {
+                if (times[i] == MAX)
+                    sb.append(-1);
+                else
+                    sb.append(times[i]);
+                sb.append("\n");
+            }
+
+            System.out.println(sb);
+        }
+        else
+            System.out.println(-1);
+    }
+
+    private static boolean bellmanFord(Edge[] edges, long[] times, int n) {
+        times[1] = 0;
+
+        for (int count = 0; count < n; count++)
+            for (Edge now : edges) {
+                if (times[now.s] == MAX)
+                    continue;
+
+                long newTime = times[now.s] + now.t;
+                if (times[now.e] <= newTime)
+                    continue;
+
+                times[now.e] = newTime;
+            }
+
+        for (Edge now : edges) {
+            if (times[now.s] == MAX)
+                continue;
+
+            long newTime = times[now.s] + now.t;
+            if (times[now.e] <= newTime)
+                continue;
+
+            return false;
+        }
+
+        return true;
+    }
+}
