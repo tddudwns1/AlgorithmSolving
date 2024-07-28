afb66890e641a75a188f9606eebcabc2baf01930
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-07-28 20:01:59 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -2,28 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * dp를 요구하는 문제
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[][] home = new int[n + 1][n + 1];
+        // 집에 벽 배치 정보
+        // 한 칸씩 늘려 dp에서 고려 사항을 줄임 (y == 0 일 때, 천장이 없으면 에러)
+        char[][] home = new char[n + 1][n + 1];
         for (int y = 1; y <= n; y++) {
+            // gpt가 StringTokenizer + Integer.parseInt보다
+            // String의 str.charAt이 더 빠를거라 해줘서 작성한 코드
+            // 그냥 빠르다고 해서 사유는 모르고
+            // 한 자리 숫자에 띄어쓰기로 구분되어 주어지는 경우에 유용할 듯
+            // 이 문제의 경우 굳이 숫자로 할 필요 없긴 함
             String str = br.readLine();
             for (int x = 1, cnt = 0; x <= n; x++, cnt += 2)
-                home[y][x] = str.charAt(cnt) - '0';
+                home[y][x] = str.charAt(cnt);
         }
 
         System.out.println(dp(n, home));
     }
 
-    private static int dp(int n, int[][] home) {
+    private static int dp(int n, char[][] home) {
         int[][][] dp = new int[n + 1][n + 1][3];
         dp[1][2][0] = 1;
 
         for (int y = 1; y <= n; y++) {
             for (int x = 3; x <= n; x++) {
-                if (home[y][x] == 1)
+                if (home[y][x] == '1')
                     continue;
 
                 int left = x - 1;
@@ -35,7 +45,7 @@ public class Main {
                 dp[y][x][2] = dp[up][x][1]
                         + dp[up][x][2];
 
-                if (home[up][x] == 1 || home[y][left] == 1)
+                if (home[up][x] == '1' || home[y][left] == '1')
                     continue;
 
                 dp[y][x][1] = dp[up][left][0]
