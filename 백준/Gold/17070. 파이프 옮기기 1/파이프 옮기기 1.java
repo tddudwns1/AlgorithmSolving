38022c1ea765a561fd36284b2488ec370f1aac13
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-07-28 19:08:39 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 84 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,71 +1,49 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-    static int N;
-    static int[][] map;
-    static int ans;
- 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
- 
-        N = Integer.parseInt(br.readLine());
-        map = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] home = new int[n + 1][n + 1];
+        for (int y = 1; y <= n; y++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int x = 1; x <= n; x++)
+                home[y][x] = Integer.parseInt(st.nextToken());
         }
- 
-        ans = 0;
-        DFS(1, 2, 0);
- 
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+
+        System.out.println(dp(n, home));
     }
- 
-    // x는 세로, y는 가로
-    // direction이 0일 때는 파이프가 가로 방향, 1일 때는 세로 방향, 2일 때는 대각선 방향.
-    public static void DFS(int x, int y, int direction) {
-        if (x == N && y == N) { // 종료 조건
-            ans++;
-            return;
+
+    private static int dp(int n, int[][] home) {
+        int[][][] dp = new int[n + 1][n + 1][3];
+        dp[1][2][0] = 1;
+
+        for (int y = 1; y <= n; y++) {
+            for (int x = 3; x <= n; x++) {
+                if (home[y][x] == 1)
+                    continue;
+
+                dp[y][x][0] = dp[y][x - 1][0]
+                        + dp[y][x - 1][1];
+
+                dp[y][x][2] = dp[y - 1][x][1]
+                        + dp[y - 1][x][2];
+
+                if (home[y - 1][x] == 1 || home[y][x - 1] == 1)
+                    continue;
+
+                dp[y][x][1] = dp[y - 1][x - 1][0]
+                        + dp[y - 1][x - 1][1]
+                        + dp[y - 1][x - 1][2];
+            }
         }
- 
-        switch (direction) {
-        case 0: // 파이프가 가로 방향일 경우, 갈 수 있는 경우는 동쪽과 대각선임.
-            if (y + 1 <= N && map[x][y + 1] == 0) { // 동쪽
-                DFS(x, y + 1, 0);
-            }
-            break;
-        case 1: // 파이프가 세로 방향일 경우, 갈 수 있는 경우는 남쪽과 대각선임.
-            if (x + 1 <= N && map[x + 1][y] == 0) { // 남쪽
-                DFS(x + 1, y, 1);
-            }
-            break;
-        case 2: // 파이프가 대각선일 경우, 갈 수 있는 경우는 동쪽과 남쪽, 대각선임.
-            if (y + 1 <= N && map[x][y + 1] == 0) { // 동쪽
-                DFS(x, y + 1, 0);
-            }
- 
-            if (x + 1 <= N && map[x + 1][y] == 0) { // 남쪽
-                DFS(x + 1, y, 1);
-            }
-            break;
-        }
- 
-        // 파이프가 어떤 방향이든지, 대각선은 검사해서 가장 아래로 뺐음.
-        if (y + 1 <= N && x + 1 <= N && map[x][y + 1] == 0 && map[x + 1][y] == 0 && map[x + 1][y + 1] == 0) {
-            DFS(x + 1, y + 1, 2);
-        }
+
+        return dp[n][n][0]
+                + dp[n][n][1]
+                + dp[n][n][2];
     }
- 
 }
