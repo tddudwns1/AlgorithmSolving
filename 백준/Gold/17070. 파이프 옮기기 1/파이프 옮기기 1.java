4f222f3c53cfed44e744564aaab3d9fefcd77c5b
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-07-28 19:36:06 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,9 +9,9 @@ public class Main {
 
         int[][] home = new int[n + 1][n + 1];
         for (int y = 1; y <= n; y++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int x = 1; x <= n; x++)
-                home[y][x] = Integer.parseInt(st.nextToken());
+            String str = br.readLine();
+            for (int x = 1, cnt = 0; x <= n; x++, cnt += 2)
+                home[y][x] = str.charAt(cnt) - '0';
         }
 
         System.out.println(dp(n, home));
@@ -26,15 +25,12 @@ public class Main {
             for (int x = 3; x <= n; x++) {
                 if (home[y][x] == 1)
                     continue;
-                
+
                 int left = x - 1;
                 int up = y - 1;
 
-                dp[y][x][0] = dp[y][left][0]
-                        + dp[y][left][1];
-
-                dp[y][x][2] = dp[up][x][1]
-                        + dp[up][x][2];
+                dp[y][x][0] = dp[y][left][0] + dp[y][left][1];
+                dp[y][x][2] = dp[up][x][1] + dp[up][x][2];
 
                 if (home[up][x] == 1 || home[y][left] == 1)
                     continue;
