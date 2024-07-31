d9c1a6d7f71a3d1448f681fbf3f15c6a3b71ec75
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-07-31 14:09:49 +0900

    [Gold IV] Title: 돌 그룹, Time: 96 ms, Memory: 16280 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.java
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.java
@@ -11,7 +11,7 @@ import java.util.StringTokenizer;
  * if b > a
  * a = a + a, b = b - a, c = c
  * a + b + c = a + a + b - a + c
- *           = a + b + c
+ * = a + b + c
  */
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -30,9 +30,10 @@ public class Main {
         if ((stone[0] + stone[1] + stone[2]) % 3 != 0)
             return 0;
 
-        // 두 수만 체크한다면 
+        // 두 수만 체크한다면 남은 수는 확정이므로 2차원 배열로 충분
         boolean[][] visited = new boolean[1501][1501];
 
+        // 세 수를 저장할 자료구조
         Queue<int[]> q = new ArrayDeque<>();
         q.add(stone);
 
@@ -43,16 +44,20 @@ public class Main {
             int b = now[1];
             int c = now[2];
 
+            // 만약 세 그룹의 수가 같다면 멈춤
             if (a == b && a == c)
                 return 1;
 
+            // a와 b가 다르다면
             if (a != b)
+                // a가 작다면
                 if (a < b)
                     addQ(a, b, c, q, visited);
+                // b가 작다면
                 else
                     addQ(b, a, c, q, visited);
 
-
+            // 이하 동일
             if (a != c)
                 if (a < c)
                     addQ(a, c, b, q, visited);
@@ -70,20 +75,27 @@ public class Main {
         return 0;
     }
 
+    /**
+     * 큐에 넣을 수 있는지 확인하는 메서드
+     * 작은 수가 350을 넘는지(너무 큰 두 수끼리의 비교를 막기 위함)
+     * 확인한 적 있는 조합인지
+     * 둘 다 해당하지 않으면 큐에 넣음
+     * @param small 작지만 바꿀 수
+     * @param big 크지만 바꿀 수
+     * @param other 남은 수
+     * @param q 세 수를 저장할 자료구조
+     * @param visited 확인한 적 있는지 판단할 배열
+     */
     private static void addQ(int small, int big, int other, Queue<int[]> q, boolean[][] visited) {
+        // 작은 수가 350을 넘는지(너무 큰 두 수끼리의 비교를 막기 위함)
         if (small > 350)
             return;
 
         int doubleSmall = small << 1;
         int bigMinusSmall = big - small;
 
+        // 확인한 적 있는 조합인지 x3
         if (visited[doubleSmall][bigMinusSmall])
-            return;
-
-        if (visited[doubleSmall][other])
-            return;
-
-        if (visited[other][bigMinusSmall])
             return;
 
         visited[doubleSmall][bigMinusSmall] = true;
@@ -92,5 +104,4 @@ public class Main {
 
         q.add(new int[]{doubleSmall, bigMinusSmall, other});
     }
-
 }
