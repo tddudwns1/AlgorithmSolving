fbb52ba4e4058e566573d0e20c3854e5e2f3db06
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-08-01 11:11:54 +0900

    [Gold V] Title: 자두나무, Time: 64 ms, Memory: 11696 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -12,30 +12,32 @@ public class Main {
         int w = Integer.parseInt(st.nextToken());
 
         int[][] dp = new int[w + 2][t + 1];
+        int benchmark = w + 1;
 
         for (int i = 1; i <= t; i++) {
             dp[0][i] += dp[0][i - 1];
             if (Integer.parseInt(br.readLine()) == 1) {
                 dp[0][i]++;
-                dp[w + 1][i]++;
+                dp[benchmark][i]++;
             }
         }
 
         for (int time = 1; time <= t; time++) {
-            for (int moveCount = 1; moveCount <= w; moveCount += 2) {
-                dp[moveCount][time] = Math.max(dp[moveCount][time - 1], dp[moveCount - 1][time - 1]);
-                if (dp[w + 1][time] == 0)
+            int before = time - 1;
+            for (int moveCount = 1; moveCount < benchmark; moveCount += 2) {
+                dp[moveCount][time] = Math.max(dp[moveCount][before], dp[moveCount - 1][before]);
+                if (dp[benchmark][time] == 0)
                     dp[moveCount][time]++;
             }
-            for (int moveCount = 2; moveCount <= w; moveCount += 2) {
-                dp[moveCount][time] = Math.max(dp[moveCount][time - 1], dp[moveCount - 1][time - 1]);
-                if (dp[w + 1][time] == 1)
+            for (int moveCount = 2; moveCount < benchmark; moveCount += 2) {
+                dp[moveCount][time] = Math.max(dp[moveCount][before], dp[moveCount - 1][before]);
+                if (dp[benchmark][time] == 1)
                     dp[moveCount][time]++;
             }
         }
 
         int answer = 0;
-        for (int i = 0; i <= w; i++)
+        for (int i = 0; i < benchmark; i++)
             answer = Math.max(answer, dp[i][t]);
 
         System.out.println(answer);
