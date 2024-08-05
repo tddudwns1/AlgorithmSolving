20ffac9fd69322575eb506b07302c7c3e26eea68
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-08-05 11:44:55 +0900

    [Gold IV] Title: 단어 섞기, Time: 408 ms, Memory: 31896 KB -BaekjoonHub

--- 백준/Gold/9177. 단어 섞기/단어 섞기.java
+++ 백준/Gold/9177. 단어 섞기/단어 섞기.java
@@ -17,47 +17,50 @@ public class Main {
             sb.append(HEAD).append(i);
 
             StringTokenizer st = new StringTokenizer(br.readLine());
-            String s1 = st.nextToken();
-            String s2 = st.nextToken();
-            String s3 = st.nextToken();
+            char[] s1 = st.nextToken().toCharArray();
+            char[] s2 = st.nextToken().toCharArray();
+            char[] s3 = st.nextToken().toCharArray();
 
-            int s1len = s1.length();
-            int s2len = s2.length();
+            int s1len = s1.length;
+            int s2len = s2.length;
             int min = Math.min(s1len, s2len);
 
             boolean[][] dp = new boolean[s1len + 1][s2len + 1];
             dp[0][0] = true;
             for (int x = 0; x < s2len; x++)
-                if (s2.charAt(x) == s3.charAt(x))
+                if (s2[x] == s3[x])
                     dp[0][x + 1] = true;
                 else
                     break;
 
             for (int y = 0; y < s1len; y++)
-                if (s1.charAt(y) == s3.charAt(y))
+                if (s1[y] == s3[y])
                     dp[y + 1][0] = true;
                 else
                     break;
 
+            int before = 0;
             for (int depth = 1; depth <= min; depth++) {
-                if (dp[depth][depth - 1] && s2.charAt(depth - 1) == s3.charAt(depth * 2 - 1))
+                if (dp[depth][before] && s2[before] == s3[depth * 2 - 1])
                     dp[depth][depth] = true;
-                else if (dp[depth - 1][depth] && s1.charAt(depth - 1) == s3.charAt(depth * 2 - 1))
+                else if (dp[before][depth] && s1[depth - 1] == s3[depth * 2 - 1])
                     dp[depth][depth] = true;
 
-                for (int x = 1; x <= s2len; x++) {
-                    if (dp[depth][x - 1] && s2.charAt(x - 1) == s3.charAt(depth + x - 1))
-                        dp[depth][x] = true;
-                    else if (dp[depth - 1][x] && s1.charAt(depth - 1) == s3.charAt(depth + x - 1))
-                        dp[depth][x] = true;
+                for (int x = 0; x < s2len; x++) {
+                    if (dp[depth][x] && s2[x] == s3[depth + x])
+                        dp[depth][x + 1] = true;
+                    else if (dp[before][x + 1] && s1[before] == s3[depth + x])
+                        dp[depth][x + 1] = true;
                 }
 
-                for (int y = 1; y <= s1len; y++) {
-                    if (dp[y - 1][depth] && s1.charAt(y - 1) == s3.charAt(depth + y - 1))
-                        dp[y][depth] = true;
-                    else if (dp[y][depth - 1] && s2.charAt(depth - 1) == s3.charAt(depth + y - 1))
-                        dp[y][depth] = true;
+                for (int y = 0; y < s1len; y++) {
+                    if (dp[y][depth] && s1[y] == s3[depth + y])
+                        dp[y + 1][depth] = true;
+                    else if (dp[y + 1][before] && s2[before] == s3[depth + y])
+                        dp[y + 1][depth] = true;
                 }
+
+                before = depth;
             }
 
             if (dp[s1len][s2len])
