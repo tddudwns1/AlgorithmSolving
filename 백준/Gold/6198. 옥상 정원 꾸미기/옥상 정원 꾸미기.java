6450705e0f7ab13c181572e5064c0038a1cb4bc0
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-08-13 06:21:29 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 196 ms, Memory: 20148 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -1,31 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        int[] stack = new int[n];
         long count = 0;
-
-        Deque<Integer> dq = new ArrayDeque<>();
+        int top = 0;
 
         for (int i = 0; i < n; i++) {
             int now = Integer.parseInt(br.readLine());
-
-            while (!dq.isEmpty() && dq.peekLast() <= now) {
-                dq.removeLast();
-                count += dq.size();
-            }
-
-            dq.addLast(now);
+            
+            while (top > 0 && stack[top - 1] <= now)
+                top--;
+            
+            count += top;
+            stack[top++] = now;
         }
-
-        for (int i = dq.size() - 1; i >= 1; i--)
-            count += i;
 
         System.out.println(count);
     }
