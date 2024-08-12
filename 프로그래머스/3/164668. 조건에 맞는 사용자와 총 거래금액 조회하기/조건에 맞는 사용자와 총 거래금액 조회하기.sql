9c9cbd785583357589b9d8e8d76f9c3a2edebd77
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-08-13 07:38:03 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -0,0 +1,15 @@
+SELECT
+    u.USER_ID,
+    u.NICKNAME,
+    sum(b.PRICE) as TOTAL_SALES
+from
+    USED_GOODS_BOARD b
+    join USED_GOODS_USER u on b.WRITER_ID = u.USER_ID
+where
+    b.STATUS = "DONE"
+group by
+    u.USER_ID
+having
+    TOTAL_SALES >= 700000
+order by
+    TOTAL_SALES
