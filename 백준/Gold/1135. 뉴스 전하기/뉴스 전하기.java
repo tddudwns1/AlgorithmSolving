28d14d9140e6a372156a5fd9d652b694c6351fc2
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-08-16 17:33:21 +0900

    [Gold II] Title: 뉴스 전하기, Time: 64 ms, Memory: 11672 KB -BaekjoonHub

--- 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.java
+++ 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.java
@@ -1,9 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
+/**
+ * 그리디 활용
+ */
 public class Main {
+    /**
+     * 직원 클래스
+     * 보스의 번호와 부하가 전파하는 최소 시간을 저장
+     */
     static class Employee {
         int boss;
         List<Integer> underlingCount = new ArrayList<>();
@@ -15,37 +25,41 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 직원의 수
         int n = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+        // 직원 정보를 기록할 배열
         Employee[] employees = new Employee[n];
         for (int i = 0; i < n; i++)
+            // 보스 번호를 저장
             employees[i] = new Employee(Integer.parseInt(st.nextToken()));
 
-        for (int i = n - 1; i > 0; i--) {
-            if (employees[i].underlingCount.isEmpty()) {
-                employees[employees[i].boss].underlingCount.add(0);
-            } else {
-                Collections.sort(employees[i].underlingCount, Collections.reverseOrder());
-                int length = employees[i].underlingCount.size();
+        // 말단 직원부터 올라오며 최소 전파 시간 계산
+        for (int i = n - 1; i > 0; i--)
+            employees[employees[i].boss].underlingCount.add(getMinTime(employees[i]));
 
-                int max = 0;
+        System.out.println(getMinTime(employees[0]));
+    }
 
-                for (int j = 0; j < length; j++)
-                    max = Math.max(employees[i].underlingCount.get(j) + j, max);
-
-                employees[employees[i].boss].underlingCount.add(max + 1);
-            }
-        }
-
-        Collections.sort(employees[0].underlingCount, Collections.reverseOrder());
-        int length = employees[0].underlingCount.size();
+    /**
+     * 최소 전파 시간 계산 메서드
+     *  1. 자신의 부하 직원들이 계산한 자신들의 최소 전파 시간 목록을 확인한다
+     *  2. 가장 오래 걸리는 직원부터 전파한다
+     *  3. 해당 시간과 그 직원에게 전파하는 시간을 합해 비교한다
+     *  4. 그 중 가장 오래 걸리는 시간 기준으로 자신에게 전파 될 시간 + 1을 보스에게 전달한다
+     * @param employees
+     * @return
+     */
+    private static int getMinTime(Employee employees) {
+        // 전파 받은 시간을 오래 걸리는 시간 순으로 정렬
+        Collections.sort(employees.underlingCount, Collections.reverseOrder());
 
         int max = -1;
 
-        for (int j = 0; j < length; j++)
-            max = Math.max(employees[0].underlingCount.get(j) + j, max);
-
-        System.out.println(max + 1);
+        for (int j = 0; j < employees.underlingCount.size(); j++)
+            max = Math.max(employees.underlingCount.get(j) + j, max);
+        
+        return max + 1;
     }
 }
