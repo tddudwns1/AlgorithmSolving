46f98adb93be4fc0f9596d81aa29686ac321d017
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-08-18 21:15:15 +0900

    [Platinum III] Title: 교수님은 기다리지 않는다, Time: 1096 ms, Memory: 312300 KB -BaekjoonHub

---
+++ 백준/Platinum/3830. 교수님은 기다리지 않는다/교수님은 기다리지 않는다.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Sample {
+        int parent;
+        long diffFromRoot = 0;
+
+        public Sample(int parent) {
+            this.parent = parent;
+        }
+    }
+
+    final static String UNKNOWN = "UNKNOWN\n";
+    static Sample[] samples;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            if (n == 0)
+                break;
+
+            int m = Integer.parseInt(st.nextToken());
+
+            samples = new Sample[n + 1];
+            for (int i = 1; i <= n; i++)
+                samples[i] = new Sample(i);
+
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+
+                char signal = st.nextToken().charAt(0);
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                switch (signal) {
+                    case '!':
+                        int w = Integer.parseInt(st.nextToken());
+                        union(a, b, w);
+                        break;
+                    case '?':
+                        if (find(a) == find(b))
+                            sb.append(samples[b].diffFromRoot - samples[a].diffFromRoot).append("\n");
+                        else
+                            sb.append(UNKNOWN);
+                        break;
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    static int find(int n) {
+        if (n == samples[n].parent)
+            return n;
+
+        int root = find(samples[n].parent);
+        samples[n].diffFromRoot += samples[samples[n].parent].diffFromRoot;
+
+        return samples[n].parent = root;
+    }
+
+    static void union(int a, int b, int w) {
+        int newA = find(a);
+        int newB = find(b);
+
+        if (newA != newB) {
+            samples[newB].parent = newA;
+            samples[newB].diffFromRoot = samples[a].diffFromRoot - samples[b].diffFromRoot + w;
+        }
+    }
+}
