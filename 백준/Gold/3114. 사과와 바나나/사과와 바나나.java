6b2db2f77e105f9835340c442776d4d6fa732147
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-08-26 21:54:26 +0900

    [Gold II] Title: 사과와 바나나, Time: 612 ms, Memory: 231160 KB -BaekjoonHub

--- 백준/Gold/3114. 사과와 바나나/사과와 바나나.java
+++ 백준/Gold/3114. 사과와 바나나/사과와 바나나.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * dp와 prefix sum 문제
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,6 +14,8 @@ public class Main {
         int r = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
 
+        // 각 과일 현황 저장 배열
+        // 추후 dp를 위해 앞 뒤 여유를 둠
         int[][] apples = new int[r + 2][c + 2];
         int[][] bananas = new int[r + 2][c + 2];
 
@@ -29,11 +34,31 @@ public class Main {
             }
         }
 
+        // 각 과일 누적합을 저장
         prefixSum(r, c, apples, bananas);
 
+        // 전체 최대 갯수 계산
         System.out.println(dp(r, c, apples, bananas));
     }
 
+    /**
+     * 각 과일의 방향별 누적합 저장 배열
+     *
+     * 불도저는 우측과 하단으로만 이동 가능
+     * 그말은 즉, 우측으로 이동 시 좌측의 땅엔 재방문 불가
+     *
+     * 우측 이동 = 좌측 하단은 A나라가 됨
+     *         = 사과 수만 세면 됨
+     * 하단 이동 = 우측 상단은 B나라가 됨
+     *         = 바나나 수만 세면 됨
+     *
+     * 누적합 적용 근거
+     *      반복되는 과일 갯수 카운팅
+     * @param r 세로
+     * @param c 가로
+     * @param apples 사과 현황
+     * @param bananas 바나나 현황
+     */
     private static void prefixSum(int r, int c, int[][] apples, int[][] bananas) {
         for (int x = 1; x <= c; x++)
             for (int y = r - 1; y > 1; y--)
@@ -44,6 +69,15 @@ public class Main {
                 bananas[y][x] += bananas[y][x + 1];
     }
 
+    /**
+     * 누적 현황을 이용한 dp 메서드
+     * 누적합 메서드의 설명이 사실상 이 코드의 설명
+     * @param r
+     * @param c
+     * @param apples
+     * @param bananas
+     * @return
+     */
     private static int dp(int r, int c, int[][] apples, int[][] bananas) {
         int[][] dp = new int[r + 1][c + 1];
 
