cc198f187b45fec79cb733b6a98c32131b1693ea
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-08-31 01:06:13 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,20 @@
+WITH RECURSIVE hours AS (
+    SELECT 0 AS HOUR
+    UNION ALL
+    SELECT HOUR + 1
+    FROM hours
+    WHERE HOUR < 23
+)
+SELECT
+    h.HOUR,
+    COALESCE(COUNT(a.ANIMAL_ID), 0) AS COUNT
+FROM
+    hours h
+LEFT JOIN
+    ANIMAL_OUTS a
+ON
+    h.HOUR = HOUR(a.DATETIME)
+GROUP BY
+    h.HOUR
+ORDER BY
+    h.HOUR;
