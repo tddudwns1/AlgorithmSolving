74f33548e0196a892557fb4ca75762e2fd0c7309
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-09-05 22:46:43 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 68 ms, Memory: 11804 KB -BaekjoonHub

---
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] restSpot = new int[n];
+        for (int i = 0; i < n; i++) {
+            restSpot[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(restSpot);
+
+        System.out.println(process(n, m, l, restSpot));
+    }
+
+    private static int process(int n, int m, int l, int[] restSpot) {
+        int left = 1;
+        int right = l * 2 / (m + 1) + 1;
+
+        int answer = Integer.MAX_VALUE;
+
+        while (left < right) {
+            int distance = (left + right) >> 1;
+
+            int count = 0;
+            int spot = 0;
+
+            int max = distance;
+            for (int i = 0; i < n; i++) {
+                int now = restSpot[i];
+
+                while (spot + distance < now && spot + distance < l) {
+                    spot += distance;
+                    count++;
+                }
+
+                max = Math.max(max, restSpot[i] - spot);
+
+                spot = restSpot[i];
+            }
+
+            while (spot + distance < l) {
+                spot += distance;
+                count++;
+            }
+
+
+            if (count > m) {
+                left = distance + 1;
+            } else {
+                right = distance;
+                answer = Math.min(answer, max);
+            }
+        }
+
+        return answer;
+    }
+}
