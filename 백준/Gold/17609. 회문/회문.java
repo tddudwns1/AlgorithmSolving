a080c9de6844e58031af0c983fe9f68556135be5
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-09-11 22:32:07 +0900

    [Gold V] Title: 회문, Time: 188 ms, Memory: 36976 KB -BaekjoonHub

---
+++ 백준/Gold/17609. 회문/회문.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static String str;
+    static int left, right;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            str = br.readLine();
+
+            left = 0;
+            right = str.length() - 1;
+
+            sb.append(isPalindrome());
+        }
+
+        System.out.print(sb);
+    }
+
+    private static String isPalindrome() {
+        if (checkCharacter())
+            return "0\n";
+
+        int tempLeft = left;
+        int tempRight = right;
+        if (str.charAt(left + 1) == str.charAt(right)) {
+            left++;
+            if (checkCharacter())
+                return "1\n";
+        }
+
+        left = tempLeft;
+        right = tempRight;
+        if (str.charAt(left) == str.charAt(right - 1)) {
+            right--;
+            if (checkCharacter())
+                return "1\n";
+        }
+
+        return "2\n";
+    }
+
+    private static boolean checkCharacter() {
+        while (str.charAt(left) == str.charAt(right)) {
+            left++;
+            right--;
+
+            if (left >= right)
+                return true;
+        }
+
+        return false;
+    }
+}
