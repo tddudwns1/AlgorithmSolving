bb711a5e2dad6013f78a8d8801f807860bc7ced0
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-11 08:21:26 +0900

    [Gold IV] Title: 서강그라운드, Time: 68 ms, Memory: 11848 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.java
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -1,10 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static class Node implements Comparable<Node> {
+        int target;
+        int distance;
+
+        public Node(int target, int distance) {
+            this.target = target;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return distance - o.distance;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,11 +33,9 @@ public class Main {
             itemCounts[i] = Integer.parseInt(st.nextToken());
         }
 
-        int maxDistance = Integer.MAX_VALUE - 15 * 100;
-        int[][] distances = new int[n + 1][n + 1];
+        List<Node>[] distances = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
-            Arrays.fill(distances[i], maxDistance);
-            distances[i][i] = 0;
+            distances[i] = new ArrayList<>();
         }
         for (int i = 0; i < r; i++) {
             st = new StringTokenizer(br.readLine());
@@ -32,36 +44,43 @@ public class Main {
             int b = Integer.parseInt(st.nextToken()); // 지역 b
             int l = Integer.parseInt(st.nextToken()); // 길의 길이
 
-            distances[a][b] = l;
-            distances[b][a] = l;
-        }
-
-        for (int target = 1; target <= n; target++) {
-            for (int start = 1; start <= n; start++) {
-                for (int end = 1; end <= n; end++) {
-                    if (distances[start][target] == maxDistance)
-                        continue;
-                    if (distances[target][end] == maxDistance)
-                        continue;
-                    int newWay = distances[start][target] + distances[target][end];
-                    if (distances[start][end] > newWay) {
-                        distances[start][end] = newWay;
-                    }
-                }
-            }
+            distances[a].add(new Node(b, l));
+            distances[b].add(new Node(a, l));
         }
 
         int answer = 0;
-        for (int start = 1; start <= n; start++) {
-            int total = 0;
-            for (int end = 1; end <= n; end++) {
-                if (distances[start][end] <= m) {
-                    total += itemCounts[end];
-                }
-            }
-            answer = Math.max(answer, total);
+        for (int i = 1; i <= n; i++) {
+            answer = Math.max(answer, djikstra(i, n, m, distances, itemCounts));
         }
 
         System.out.println(answer);
+    }
+
+    private static int djikstra(int start, int n, int m, List<Node>[] distances, int[] itemCounts) {
+        boolean[] visited = new boolean[n + 1];
+
+        Queue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+
+        int total = 0;
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            if (visited[now.target])
+                continue;
+            visited[now.target] = true;
+
+            total += itemCounts[now.target];
+
+            for (Node next : distances[now.target]) {
+                int newDistance = now.distance + next.distance;
+                if (newDistance > m)
+                    continue;
+
+                pq.add(new Node(next.target, newDistance));
+            }
+        }
+
+        return total;
     }
 }
