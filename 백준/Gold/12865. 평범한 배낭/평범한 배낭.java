5c20cb59f8c519543f249c6bec124dd5c04846f1
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-16 21:59:07 +0900

    [Gold V] Title: 평범한 배낭, Time: 100 ms, Memory: 12252 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,27 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		int[] knapsack = new int[k + 1];
+public class Main {
+    static class Product implements Comparable<Product> {
+        int w;
+        int v;
 
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			int w = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			for (int j = k; j > 0; j--) {
-				if (j < w)
-					break;
-				if (knapsack[j] < knapsack[j - w] + v)
-					knapsack[j] = knapsack[j - w] + v;
-			}
-		}
-		System.out.println(knapsack[k]);
-	}
+        public Product(int w, int v) {
+            this.w = w;
+            this.v = v;
+        }
+
+        @Override
+        public int compareTo(Product o) {
+            return w - o.w;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Queue<Product> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            pq.add(new Product(w, v));
+        }
+
+        int[] cost = new int[k + 1];
+        while (!pq.isEmpty()) {
+            Product now = pq.poll();
+
+            for (int i = k; i >= now.w; i--) {
+                if (cost[i] < cost[i - now.w] + now.v)
+                    cost[i] = cost[i - now.w] + now.v;
+            }
+        }
+
+        System.out.println(cost[k]);
+    }
 }
