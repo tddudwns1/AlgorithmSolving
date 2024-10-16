fd45756133e6f7697dbe45291b7f142884c6c34b
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-16 12:59:33 +0900

    [Gold I] Title: 외판원 순회, Time: 112 ms, Memory: 16264 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -1,45 +1,55 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] map, dp;
-	static int n, end, INF = 1600_0000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		map = new int[n][n];
-		dp = new int[n][1 << n];
-		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < n; j++)
-				map[i][j] = Integer.parseInt(st.nextToken());
-		}
-		end = (1 << n--) - 1;
+        int[][] costs = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                costs[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		System.out.println(dfs(0, 1));
-	}
+        System.out.println(process(n, costs));
+    }
 
-	private static int dfs(int now, int visited) {
-		if (visited == end) {
-			if (map[now][0] == 0)
-				return 1600_0001;
-			return map[now][0];
-		}
+    private static int process(int n, int[][] costs) {
+        int allVisited = (1 << n) - 1;
+        int lastCountryNumber = n - 1;
+        int maxCost = 16 * 1_000_000 + 1;
 
-		if (dp[now][visited] != 0)
-			return dp[now][visited];
+        int[][] visited = new int[n][allVisited];
 
-		int min = INF;
-		for (int i = n; i >= 0; i--) {
-			if ((visited & (1 << i)) > 0)
-				continue;
-			if (map[now][i] == 0)
-				continue;
-			min = Math.min(min, map[now][i] + dfs(i, visited | (1 << i)));
-		}
-		
-		return dp[now][visited] = min;
-	}
+        return dfs(0, 1, visited, costs, allVisited, lastCountryNumber, maxCost);
+    }
+
+    private static int dfs(int now, int nowVisited, int[][] visited, int[][] costs, int allVisited, int lastCountryNumber, int maxCost) {
+        if (nowVisited == allVisited) {
+            if (costs[now][0] == 0)
+                return maxCost;
+            return costs[now][0];
+        }
+
+        if (visited[now][nowVisited] != 0)
+            return visited[now][nowVisited];
+
+        int minCost = maxCost;
+
+        for (int i = lastCountryNumber; i >= 0; i--) {
+            int nowCountry = 1 << i;
+            if ((nowVisited & nowCountry) != 0)
+                continue;
+            if (costs[now][i] == 0)
+                continue;
+
+            minCost = Math.min(minCost, costs[now][i] + dfs(i, nowVisited | nowCountry, visited, costs, allVisited, lastCountryNumber, maxCost));
+        }
+        return visited[now][nowVisited] = minCost;
+    }
 }
