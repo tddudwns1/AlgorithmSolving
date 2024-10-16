ca3907e84368a70956da87b111531917e5b07ee6
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-16 23:30:58 +0900

    [Gold IV] Title: 호텔, Time: 68 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int c = Integer.parseInt(st.nextToken()) + 100;
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] costs = new int[c];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        costs[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < c - w; j++) {
+                if (costs[j] != Integer.MAX_VALUE)
+                    costs[j + w] = Math.min(costs[j + w], costs[j] + v);
+            }
+        }
+
+        int answer = Integer.MAX_VALUE;
+        for (int j = c - 100; j < c; j++) {
+            answer = Math.min(answer, costs[j]);
+        }
+
+        System.out.println(answer);
+    }
+}
