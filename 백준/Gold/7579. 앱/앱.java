f1f0f3baa279120ee9f6b31c7b9a289faf104ca4
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-30 16:28:49 +0900

    [Gold III] Title: 앱, Time: 2424 ms, Memory: 90700 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] weights = new int[2000_0001];
+        Arrays.fill(weights, 10_000_001);
+        weights[0] = 0;
+
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+        for (int app = 0; app < n; app++) {
+            int memory = Integer.parseInt(st1.nextToken());
+            int weight = Integer.parseInt(st2.nextToken());
+
+            for (int nowMemory = 2000_0000; nowMemory >= memory; nowMemory--) {
+                int newWeight = weights[nowMemory - memory] + weight;
+                if (weights[nowMemory] <= newWeight)
+                    continue;
+
+                weights[nowMemory] = newWeight;
+            }
+        }
+
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = m; i <= 2000_0000; i++) {
+            answer = Math.min(answer, weights[i]);
+        }
+
+        System.out.println(answer);
+    }
+}
