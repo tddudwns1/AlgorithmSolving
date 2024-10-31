8b6c6665245a111544ffa4efa43ba196a465a481
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-31 22:26:19 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,17 +1,18 @@
 select
-    concat('/home/grep/src/', f.BOARD_ID, '/', f.FILE_ID, f.FILE_NAME, f.FILE_EXT) as FILE_PATH
+    concat("/home/grep/src/", b.BOARD_ID, "/", FILE_ID, FILE_NAME, FILE_EXT)
+    as FILE_PATH
 from
     USED_GOODS_FILE f
-    join (
+    join
+    (
         select
             BOARD_ID
         from
             USED_GOODS_BOARD
         order by
             VIEWS desc
-        limit
-            1
+        limit 1
     ) b
-        on b.BOARD_ID = f.BOARD_ID
+        on f.BOARD_ID = b.BOARD_ID
 order by
-    FILE_PATH desc
+    FILE_ID desc
