2271392865a3cbd92716306a8b5f11afa5ba3b80
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-31 16:18:52 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -4,7 +4,27 @@ select
 from
     FISH_INFO 
 order by
-    LENGTH desc,
-    ID
+    LENGTH desc, ID
 limit
     10
+
+
+
+
+
+
+
+
+
+
+
+# select
+#     ID,
+#     LENGTH
+# from
+#     FISH_INFO 
+# order by
+#     LENGTH desc,
+#     ID
+# limit
+#     10
