eb7bacd0c0d9d15d855cc1a38ee46f86daf83e5e
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-31 21:19:35 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,6 +1,6 @@
 select
-    sum(SCORE) as SCORE,
-    e.EMP_NO,
+    sum(g.SCORE) as SCORE,
+    g.EMP_NO,
     e.EMP_NAME,
     e.POSITION,
     e.EMAIL
@@ -10,13 +10,40 @@ from
     HR_GRADE g
         on e.EMP_NO = g.EMP_NO
 group by
-    g.EMP_NO
+    EMP_NO
 order by
-    SCORE desc
+    score desc
 limit
     1
 
-# 2022년도 한해 평가 점수(상,하반기 점수의 합) 가 가장 높은 사원
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+# select
+#     sum(SCORE) as SCORE,
+#     e.EMP_NO,
+#     e.EMP_NAME,
+#     e.POSITION,
+#     e.EMAIL
+# from
+#     HR_EMPLOYEES e
+#     join
+#     HR_GRADE g
+#         on e.EMP_NO = g.EMP_NO
+# group by
+#     g.EMP_NO
+# order by
+#     SCORE desc
+# limit
+#     1
