200bb60403f57ca7a61ffdc1b68f986fef85e197
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-31 21:32:00 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,10 +1,17 @@
--- 코드를 입력하세요
-SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, 
-    CASE
-        when STATUS = 'SALE' then '판매중'
-        when STATUS = 'RESERVED' then '예약중'
-        when STATUS = 'DONE' then '거래완료'
-    end as status
-from USED_GOODS_BOARD
-WHERE DATE_FORMAT(CREATED_DATE, '%Y-%m-%d') = '2022-10-05'
-order by BOARD_ID desc
+select
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    case
+        when STATUS = "SALE" then "판매중"
+        when STATUS = "RESERVED" then "예약중"
+        else "거래완료"
+    end
+    as STATUS
+from
+    USED_GOODS_BOARD
+where
+    date_format(CREATED_DATE, "%Y-%m-%d") = "2022-10-05"
+order by
+    BOARD_ID desc
