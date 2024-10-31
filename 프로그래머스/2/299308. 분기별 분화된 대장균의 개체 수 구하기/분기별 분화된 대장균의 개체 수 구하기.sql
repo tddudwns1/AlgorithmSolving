2ad9ea03ea256b8247b1d4649d9004b666b0f7af
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-31 20:40:49 +0900

    [level 2] Title: 분기별 분화된 대장균의 개체 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/299308. 분기별 분화된 대장균의 개체 수 구하기/분기별 분화된 대장균의 개체 수 구하기.sql
+++ 프로그래머스/2/299308. 분기별 분화된 대장균의 개체 수 구하기/분기별 분화된 대장균의 개체 수 구하기.sql
@@ -1,18 +1,56 @@
 select
-    s.QUARTER as QUARTER,
+    q.QUARTER,
     count(*) as ECOLI_COUNT
 from
     (
         select
             case
-                when month(DIFFERENTIATION_DATE) between 1 and 3 then '1Q'
-                when month(DIFFERENTIATION_DATE) between 4 and 6 then '2Q'
-                when month(DIFFERENTIATION_DATE) between 7 and 9 then '3Q'
-                else '4Q'
+                when month(DIFFERENTIATION_DATE) between 1 and 3 then "1Q"
+                when month(DIFFERENTIATION_DATE) between 4 and 6 then "2Q"
+                when month(DIFFERENTIATION_DATE) between 7 and 9 then "3Q"
+                else "4Q"
             end
             as QUARTER
         from
-            ECOLI_DATA 
-    ) s
+            ECOLI_DATA
+    ) q
 group by
-    s.QUARTER
+    QUARTER
+order by
+    QUARTER
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# select
+#     s.QUARTER as QUARTER,
+#     count(*) as ECOLI_COUNT
+# from
+#     (
+#         select
+#             case
+#                 when month(DIFFERENTIATION_DATE) between 1 and 3 then '1Q'
+#                 when month(DIFFERENTIATION_DATE) between 4 and 6 then '2Q'
+#                 when month(DIFFERENTIATION_DATE) between 7 and 9 then '3Q'
+#                 else '4Q'
+#             end
+#             as QUARTER
+#         from
+#             ECOLI_DATA 
+#     ) s
+# group by
+#     s.QUARTER
+# ORDER BY
+#     s.QUARTER
