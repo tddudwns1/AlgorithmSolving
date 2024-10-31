83afaa162f08bf2d3967b6966ff9f7bde81014b2
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-31 19:43:51 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,15 +1,44 @@
-select
-    ID,
+select 
+    distinct(ID),
     EMAIL,
     FIRST_NAME,
     LAST_NAME
 from
-    DEVELOPERS
-where
-    SKILL_CODE & (
-        SELECT SUM(CODE)
-        FROM SKILLCODES
-        WHERE NAME IN ('Python', 'C#')
-    ) > 0
+    DEVELOPERS d
+    join
+    SKILLCODES s
+        on s.NAME in ("Python", "C#")
+        and s.CODE & d.SKILL_CODE = s.CODE
 order by
     ID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT DISTINCT
+#     ID,
+#     EMAIL,
+#     FIRST_NAME,
+#     LAST_NAME
+# FROM
+#     DEVELOPERS
+#     JOIN
+#     SKILLCODES
+#     ON DEVELOPERS.SKILL_CODE & SKILLCODES.CODE
+# WHERE
+#     NAME="C#"
+#    OR NAME="Python"
+# ORDER BY
+#     ID
