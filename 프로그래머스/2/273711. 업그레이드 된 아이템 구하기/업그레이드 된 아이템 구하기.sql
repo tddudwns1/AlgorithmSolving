4eb764859cd988c2d56561b9bb48b75f7e056da5
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-31 19:57:31 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,19 +1,46 @@
 select
-    i.ITEM_ID,
-    i.ITEM_NAME,
-    i.RARITY
+    u.ITEM_ID,
+    u.ITEM_NAME,
+    u.RARITY
 from
     ITEM_INFO i
     join
-    (
-        select
-            t.ITEM_ID as ITEM_ID
-        from
-            ITEM_INFO i
-            join
-            ITEM_TREE t
-                on i.ITEM_ID = t.PARENT_ITEM_ID
-                and i.RARITY = 'RARE'
-    ) s on i.ITEM_ID = s.ITEM_ID
+    ITEM_TREE t
+        on i.RARITY = "RARE"
+        and i.ITEM_ID = t.PARENT_ITEM_ID
+    join
+    ITEM_INFO u
+        on t.ITEM_ID = u.ITEM_ID
+
 order by
-    i.ITEM_ID desc
+    ITEM_ID desc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# select
+#     i2.ITEM_ID,
+#     i2.ITEM_NAME,
+#     i2.RARITY
+# from
+#     ITEM_INFO i
+#     join
+#     ITEM_TREE t
+#         on i.ITEM_ID = t.PARENT_ITEM_ID
+#         and i.RARITY = 'RARE'
+#     join
+#     ITEM_INFO i2
+#         on t.ITEM_ID = i2.ITEM_ID
+# order by
+#     ITEM_ID desc
