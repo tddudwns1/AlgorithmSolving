dbb22c1c50a1d02c66586ee21a4b35e404e40b93
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-10-31 19:37:59 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -1,13 +1,42 @@
 select
-    c.ID,
-    c.GENOTYPE,
-    p.GENOTYPE & c.GENOTYPE as PARENT_GENOTYPE
+    child.ID,
+    child.GENOTYPE,
+    parent.GENOTYPE as PARENT_GENOTYPE
 from
-    ECOLI_DATA c
+    ECOLI_DATA child
     join
-    ECOLI_DATA p
-        on c.PARENT_ID = p.ID
-where
-    p.GENOTYPE & c.GENOTYPE = p.GENOTYPE
+    ECOLI_DATA parent
+        on child.PARENT_ID = parent.ID
+        and parent.GENOTYPE & child.GENOTYPE = parent.GENOTYPE
 order by
-    c.ID
+    ID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# select
+#     c.ID,
+#     c.GENOTYPE,
+#     p.GENOTYPE as PARENT_GENOTYPE
+# from
+#     ECOLI_DATA c
+#     join
+#     ECOLI_DATA p
+#         on c.PARENT_ID = p.ID
+# where
+#     p.GENOTYPE & c.GENOTYPE = p.GENOTYPE
+# order by
+#     c.ID
