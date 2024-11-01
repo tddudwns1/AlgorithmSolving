3ad752ab68f8b33c87d4775f5063255aa9263219
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-11-01 15:40:39 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,15 +1,42 @@
 select
     ID,
     case
-        when percent_rank() over (order by SIZE_OF_COLONY) <= 0.25 then "LOW"
-        when percent_rank() over (order by SIZE_OF_COLONY) <= 0.50 then "MEDIUM"
-        when percent_rank() over (order by SIZE_OF_COLONY) <= 0.75 then "HIGH"
-        else "CRITICAL"
+        when ntile(4) over (order by SIZE_OF_COLONY desc) = 1 then "CRITICAL"
+        when ntile(4) over (order by SIZE_OF_COLONY desc) = 2 then "HIGH"
+        when ntile(4) over (order by SIZE_OF_COLONY desc) = 3 then "MEDIUM"
+        else "LOW"
     end as COLONY_NAME
 from
-    ECOLI_DATA
+    ECOLI_DATA 
 order by
     ID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # select
+# #     ID,
+# #     case
+# #         when percent_rank() over (order by SIZE_OF_COLONY) <= 0.25 then "LOW"
+# #         when percent_rank() over (order by SIZE_OF_COLONY) <= 0.50 then "MEDIUM"
+# #         when percent_rank() over (order by SIZE_OF_COLONY) <= 0.75 then "HIGH"
+# #         else "CRITICAL"
+# #     end as COLONY_NAME
+# # from
+# #     ECOLI_DATA
+# # order by
+# #     ID
 
 
 
@@ -38,7 +65,6 @@ order by
 #     (
 #         select
 #             ID,
-#             count(*) OVER () as total,
 #             ntile(4) over (order by SIZE_OF_COLONY) as tile
 #         from
 #             ECOLI_DATA 
