0adb5b55d6fd080b3733ee8e2f855c68d3521414
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2024-11-02 00:45:31 +0900

    [Gold IV] Title: 부분합, Time: 184 ms, Memory: 22344 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,5 +1,6 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -17,21 +18,20 @@ public class Main {
 
         int answer = 0;
         int shortest = Integer.MAX_VALUE;
-        int len = 0;
         int sum = 0;
 
         int left = 0;
         int right = 0;
-        while(true){
-            if(sum < s){
-                if(right == n)
+        while (true) {
+            if (sum < s) {
+                if (right == n)
                     break;
                 sum += sequence[right++];
-                len++;
-            }else{
-                answer = shortest = Integer.min(shortest, len);
+            } else {
+                answer = shortest = Integer.min(shortest, right - left);
+                if (answer == 1)
+                    break;
                 sum -= sequence[left++];
-                len--;
             }
         }
 
