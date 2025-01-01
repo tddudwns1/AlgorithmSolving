5ac78ad785a7056355815d59c746180a5357d099
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2025-01-02 06:56:33 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 492 ms, Memory: 234956 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        char[][] mark = new char[n][2 * n - 1];
+        for (int i = 0; i < n; i++)
+            Arrays.fill(mark[i], ' ');
+
+        process(0, 0, n, mark);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2 * n - 1; j++) {
+                sb.append(mark[i][j]);
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void process(int y, int x, int len, char[][] mark) {
+        if (len == 3) {
+            markProcess(y, x, mark);
+            return;
+        }
+        int halfLen = len >> 1;
+        process(y, x + halfLen, halfLen, mark);
+        process(y + halfLen, x, halfLen, mark);
+        process(y + halfLen, x + len, halfLen, mark);
+    }
+
+    private static void markProcess(int y, int x, char[][] mark) {
+        mark[y][x + 2] = '*';
+        mark[++y][x + 1] = '*';
+        mark[y++][x + 3] = '*';
+        for (int i = 0; i < 5; i++)
+            mark[y][x + i] = '*';
+    }
+}
