d761d1b59da8f895552f9c04ebf22931f1b903ae
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2025-01-12 17:17:59 +0900

    [Gold II] Title: 공항, Time: 184 ms, Memory: 26112 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int g = Integer.parseInt(br.readLine()); // gate 수
+        int p = Integer.parseInt(br.readLine()); // plane 수
+
+        int[] usuallyGate = new int[g + 1];
+        for (int i = 1; i <= g; i++) {
+            usuallyGate[i] = i;
+        }
+
+        int i;
+
+        for (i = 0; i < p; ++i) {
+            int n = Integer.parseInt(br.readLine()); // 사용 가능한 gate 번호
+            find(n, usuallyGate);
+            if (usuallyGate[0] == -1) {
+                break;
+            }
+        }
+
+        System.out.println(i);
+    }
+
+    private static int find(int n, int[] usuallyGate) {
+        if (n == usuallyGate[n]) {
+            return usuallyGate[n] = n - 1;
+        }
+
+        return usuallyGate[n] = find(usuallyGate[n], usuallyGate);
+    }
+}
